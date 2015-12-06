bf39cafbbace2debbb9829bd8a98664a9472457d
Mark Alba <mark.alba@nokia.com>
2015-12-07 00:36:56 +0800

    "aprgwebcrawler fixes"

--- AprgWebCrawler/AprgWebCrawler/src/AprgWebCrawler.cpp
+++ AprgWebCrawler/AprgWebCrawler/src/AprgWebCrawler.cpp
@@ -5,10 +5,9 @@
 #include <fstream>
 #include <iostream>
 
-#define FIREFOX_EXECUTABLE_PATH R"(C:\Program Files (x86)\Mozilla Firefox\firefox.exe)"
+#define FIREFOX_EXECUTABLE_PATH R"(C:\"Program Files"\"Mozilla Firefox"\firefox.exe)"
 
 using namespace std;
-
 namespace alba
 {
 
