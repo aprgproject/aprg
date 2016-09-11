b3607c408cdff1ae6714e1e842fd613a5bffe64c
Mark Alba <mark.alba@nokia.com>
2016-09-11 10:46:45 +0800

    "modifying aprgwebcrawler filesize limts"

--- AprgWebCrawler/AprgWebCrawler/src/WebCrawler/CrawlConfiguration/CrawlConfiguration.cpp
+++ AprgWebCrawler/AprgWebCrawler/src/WebCrawler/CrawlConfiguration/CrawlConfiguration.cpp
@@ -31,11 +31,10 @@ CrawlConfiguration::CrawlConfiguration(CrawlMode mode)
     case CrawlMode::Y8:
         m_isFileToBeDownloadUntilSuccessful=true;
         m_numberOfRetries = 0;
-        m_minimumFileSize = 10000;
+        m_minimumFileSize = 1000;
         break;
     case CrawlMode::Youtube:
-        m_isFileToBeDownloadUntilSuccessful=true;
-        m_numberOfRetries = 0;
+        m_isFileToBeDownloadUntilSuccessful=true;        m_numberOfRetries = 0;
         m_minimumFileSize = 1000000;
         break;
     case CrawlMode::Empty:
