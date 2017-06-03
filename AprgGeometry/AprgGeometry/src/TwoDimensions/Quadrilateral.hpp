2fdbcf4dc1f2cf7330b39c5d97fda8a6beabf629
Mark Alba <mark.alba@nokia.com>
2017-06-04 03:13:21 +0800

    "added functionalities on aprggeometry"

--- AprgGeometry/AprgGeometry/src/TwoDimensions/Quadrilateral.hpp
+++ AprgGeometry/AprgGeometry/src/TwoDimensions/Quadrilateral.hpp
@@ -5,11 +5,10 @@
 namespace alba
 {
 
-using QuadrilateralParent = Polygon<3>;
+using QuadrilateralParent = Polygon<4>;
 
 class Quadrilateral : public QuadrilateralParent
-{
-public:
+{public:
     Quadrilateral();
     Quadrilateral(Point const& first, Point const& second, Point const& third, Point const& fourth);
 
