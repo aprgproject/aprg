9c1203f00318757dd788db1d1531fc0987e0d23d
Mark Alba <mark.alba@nokia.com>
2017-07-16 15:23:20 +0800

    "added soosa functionalities"

--- AprgStatistics/AprgStatistics/src/TwoDimensionsStatistics.hpp
+++ AprgStatistics/AprgStatistics/src/TwoDimensionsStatistics.hpp
@@ -4,20 +4,20 @@
 #include <DataStatistics.hpp>
 
 #include <map>
+#include <unordered_map>
 
 namespace alba
 {
-
 namespace TwoDimensionsStatistics
 {
     using Sample = DataSample<2>;
     using Statistics = DataStatistics<2>;
     using Samples = std::vector<Sample>;
     using ValueToSampleMultimap = std::multimap<double, Sample>;
+    using ValueToSampleUnorderedMultimap = std::unordered_multimap<double, Sample>;
     using ValueToSamplePair = std::pair<double, Sample>;
 
-    struct LineModel
-    {
+    struct LineModel    {
         double aCoefficient;
         double bCoefficient;
         double cCoefficient;
