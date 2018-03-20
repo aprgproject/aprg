a7756322949996b1ad3b9061deb8f076f6be56d6
Jobelle Mabasa <jobelle.mabasa@nokia.com>
2018-03-21 00:40:35 +0800

    "added LrmAllocationSimulator"

--- AprgCommon/AprgCmakeInclude/AprgCommon.cmake
+++ AprgCommon/AprgCmakeInclude/AprgCommon.cmake
@@ -1,12 +1,5 @@
-#Assign Definitions
-add_definitions(-DAPRG_DIR="${APRG_DIR}")
-if(WIN32)
-    add_definitions(-DOS_WINDOWS="WIN32")
-endif()
-
 #Assign AprgCommon directory, this needs aprg directory
 set(APRG_COMMON_DIR ${APRG_DIR}/AprgCommon/AprgCommon/)
-
 #Include APRG_COMMON files and directories
 include(${APRG_COMMON_DIR}/cmake/FilesAndDirectories.cmake)
 
