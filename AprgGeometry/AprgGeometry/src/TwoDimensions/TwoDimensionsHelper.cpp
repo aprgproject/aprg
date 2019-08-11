569dd5675e2f5ed08aa43de7554abf383e234c3d
Mark Alba <mark.alba@nokia.com>
2019-08-11 21:23:58 +0800

    "added aprggeometry functionalities"

--- AprgGeometry/AprgGeometry/src/TwoDimensions/TwoDimensionsHelper.cpp
+++ AprgGeometry/AprgGeometry/src/TwoDimensions/TwoDimensionsHelper.cpp
@@ -17,10 +17,14 @@ namespace TwoDimensions
 namespace twoDimensionsHelper
 {
 
+bool isInsideTwoPoints(Point const& point, Point const& minimumXAndY, Point const& maximumXAndY)
+{
+    return (point.getX() >= minimumXAndY.getX() && point.getY() >= minimumXAndY.getY() && point.getX() <= maximumXAndY.getX() && point.getY() <= maximumXAndY.getY());
+}
+
 bool isPointInLine(Point const& point, Line const& line)
 {
-    return isAlmostEqual(point.getY(), line.calculateYFromX(point.getX()));
-}
+    return isAlmostEqual(point.getY(), line.calculateYFromX(point.getX()));}
 
 double getDistance(Point const& point1, Point const& point2)
 {
@@ -35,28 +39,38 @@ double getDistance(Line const& line, Point const& point)
     return getDistance(point, nearestPoint);
 }
 
+Point getIntersection(Line const& line1, Line const& line2)
+{
+    double xOfIntersection = ((line2.getCCoefficient()*line1.getBCoefficient())-(line1.getCCoefficient()*line2.getBCoefficient()))                              /((line1.getACoefficient()*line2.getBCoefficient())-(line2.getACoefficient()*line1.getBCoefficient()));
+    double yOfIntersection = ((line2.getCCoefficient()*line1.getACoefficient())-(line1.getCCoefficient()*line2.getACoefficient()))
+            /((line1.getBCoefficient()*line2.getACoefficient())-(line2.getBCoefficient()*line1.getACoefficient()));
+    return Point(xOfIntersection, yOfIntersection);
+}
+
 Point getMidpoint(Point const& point1, Point const& point2)
 {
     return Point((point1.getX()+point2.getX())/2,  (point1.getY()+point2.getY())/2);
 }
 
-Line getLineWithSameSlope(Line const& line, Point const& point)
+Point popNearestPoint(Points & points, Point const& point)
 {
-    return Line(line.getACoefficient(), line.getBCoefficient(), -1*((line.getACoefficient()*point.getX())+(line.getBCoefficient()*point.getY())));
-}
-
-Line getLineWithPerpendicularSlope(Line const& line, Point const& point)
-{
-    return Line(line.getBCoefficient(), -line.getACoefficient(), (line.getACoefficient()*point.getY())-(line.getBCoefficient()*point.getX()));
-}
-
-Point getIntersection(Line const& line1, Line const& line2)
-{
-    double xOfIntersection = ((line2.getCCoefficient()*line1.getBCoefficient())-(line1.getCCoefficient()*line2.getBCoefficient()))
-                              /((line1.getACoefficient()*line2.getBCoefficient())-(line2.getACoefficient()*line1.getBCoefficient()));
-    double yOfIntersection = ((line2.getCCoefficient()*line1.getACoefficient())-(line1.getCCoefficient()*line2.getACoefficient()))
-            /((line1.getBCoefficient()*line2.getACoefficient())-(line2.getBCoefficient()*line1.getACoefficient()));
-    return Point(xOfIntersection, yOfIntersection);
+    Point result;    if(!points.empty())
+    {
+        double nearestDistance=getDistance(points[0], point);
+        Points::iterator nearestPointIterator = points.begin();
+        for(Points::iterator it = points.begin(); it != points.end(); it++)
+        {
+            double currentDistance(getDistance(*it, point));
+            if(nearestDistance>currentDistance)
+            {
+                nearestDistance = currentDistance;
+                nearestPointIterator = it;
+            }
+        }
+        result = *nearestPointIterator;
+        points.erase(nearestPointIterator);
+    }
+    return result;
 }
 
 Points getConnectedPointsUsingALine(Points const& inputPoints, double const interval)
@@ -78,52 +92,9 @@ Points getConnectedPointsUsingALine(Points const& inputPoints, double const inte
     return resultingPoints; //RVO
 }
 
-void savePointsFromTwoPointsUsingALineWithoutLastPoint(Points & points, Point const& previousPoint, Point const& currentPoint, double const interval)
-{
-    Line line(previousPoint, currentPoint);
-    Points pointsInLine(line.getPointsWithoutLastPoint(previousPoint, currentPoint, interval));
-    std::copy(pointsInLine.begin(), pointsInLine.end(), std::back_inserter(points));
-}
-
-Point popNearestPoint(Points & points, Point const& point)
-{
-    Point result;
-    if(!points.empty())
-    {
-        double nearestDistance=getDistance(points[0], point);
-        Points::iterator nearestPointIterator = points.begin();
-        for(Points::iterator it = points.begin(); it != points.end(); it++)
-        {
-            double currentDistance(getDistance(*it, point));
-            if(nearestDistance>currentDistance)
-            {
-                nearestDistance = currentDistance;
-                nearestPointIterator = it;
-            }
-        }
-        result = *nearestPointIterator;
-        points.erase(nearestPointIterator);
-    }
-    return result;
-}
-
-void addPointIfInsideTwoPoints(Points & pointsAtBorder, Point const& point, Point const& minimumXAndY, Point const& maximumXAndY)
-{
-    if(isInsideTwoPoints(point, minimumXAndY, maximumXAndY))
-    {
-        pointsAtBorder.emplace_back(point);
-    }
-}
-
-bool isInsideTwoPoints(Point const& point, Point const& minimumXAndY, Point const& maximumXAndY)
-{
-    return (point.getX() >= minimumXAndY.getX() && point.getY() >= minimumXAndY.getY() && point.getX() <= maximumXAndY.getX() && point.getY() <= maximumXAndY.getY());
-}
-
 Points getMergedPointsInIncreasingX(Points const& firstPointsToBeMerged, Points const& secondPointsToBeMerged)
 {
-    Points result;
-    Points firstPoints(getPointsInSortedIncreasingX(firstPointsToBeMerged));
+    Points result;    Points firstPoints(getPointsInSortedIncreasingX(firstPointsToBeMerged));
     Points secondPoints(getPointsInSortedIncreasingX(secondPointsToBeMerged));
     Points::const_iterator iteratorForX = firstPoints.cbegin();
     Points::const_iterator iteratorForY = secondPoints.cbegin();
@@ -214,10 +185,19 @@ Points getPointsInSortedDecreasingX(Points const& pointsToBeSorted)
     return result;
 }
 
+Line getLineWithSameSlope(Line const& line, Point const& point)
+{
+    return Line(line.getACoefficient(), line.getBCoefficient(), -1*((line.getACoefficient()*point.getX())+(line.getBCoefficient()*point.getY())));
+}
+
+Line getLineWithPerpendicularSlope(Line const& line, Point const& point)
+{
+    return Line(line.getBCoefficient(), -line.getACoefficient(), (line.getACoefficient()*point.getY())-(line.getBCoefficient()*point.getX()));
+}
+
 Line getTangentLineAt(Circle const& circle, Point const& point)
 {
-    Point nearestPoint(circle.getNearestPointInCircumference(point));
-    Point center(circle.getCenter());
+    Point nearestPoint(circle.getNearestPointInCircumference(point));    Point center(circle.getCenter());
     Point deltaNearestPoint(nearestPoint.getX()-center.getX(), nearestPoint.getY()-center.getY());
     return Line(deltaNearestPoint.getY(), -1*deltaNearestPoint.getX(), nearestPoint);
 }
@@ -231,6 +211,21 @@ Line getTangentLineAt(Ellipse const& ellipse, Point const& point)
 Line getTangentLineAt(Hyperbola const& hyperbola, Point const& point)
 {
     return Line(point.getX()/pow(hyperbola.getAValue(), 2), -point.getY()/pow(hyperbola.getBValue(), 2), -1);
+}
+
+void addPointIfInsideTwoPoints(Points & pointsAtBorder, Point const& point, Point const& minimumXAndY, Point const& maximumXAndY)
+{
+    if(isInsideTwoPoints(point, minimumXAndY, maximumXAndY))
+    {
+        pointsAtBorder.emplace_back(point);
+    }
+}
+
+void savePointsFromTwoPointsUsingALineWithoutLastPoint(Points & points, Point const& previousPoint, Point const& currentPoint, double const interval)
+{
+    Line line(previousPoint, currentPoint);
+    Points pointsInLine(line.getPointsWithoutLastPoint(previousPoint, currentPoint, interval));
+    std::copy(pointsInLine.begin(), pointsInLine.end(), std::back_inserter(points));
 }
 
 }
