fc39c8d7ede8933bbd13061ca49dfea8f78cec0a
mevalba <markearvinalba@gmail.com>
2020-11-01 04:43:25 +0800

    "added ro functionalities"

--- RagnarokOnline/RagnarokOnline/tst/RetrieveItemDataFromRms_unit.cpp
+++ RagnarokOnline/RagnarokOnline/tst/RetrieveItemDataFromRms_unit.cpp
@@ -7,10 +7,36 @@ using namespace std;
 namespace alba
 {
 
-TEST(RagnarokOnlineTest, RetrieveItemDataFromRmsWebPageWorksWithExample1)
+TEST(RagnarokOnlineTest, DISABLED_RetrieveItemDataFromRmsWebPagesAndThenSaveTheData)
 {
     RagnarokOnline ragnarokOnline;
 
+    ragnarokOnline.retrieveItemDataFromRmsWebpages(R"(C:\Users\detectivemark7\Desktop\RO\RMS\ItemDatabaseTraversal\)");
+
+    ragnarokOnline.saveItemIdToItemMapToFile(R"(C:\Users\detectivemark7\Desktop\RO\ItemIdToItemMap.txt)");
+}
+
+TEST(RagnarokOnlineTest, ReadItemIdToItemMapFromFileWorks)
+{
+    RagnarokOnline ragnarokOnline;
+
+    ragnarokOnline.readItemIdToItemMapFromFile(R"(C:\Users\detectivemark7\Desktop\RO\ItemIdToItemMap.txt)");
+
+    EXPECT_EQ(6225U, ragnarokOnline.getItemIdToItemMap().size());
+}
+
+TEST(RagnarokOnlineTest, RetrieveItemDataFromRmsWebPagesWorks)
+{
+    RagnarokOnline ragnarokOnline;
+
+    ragnarokOnline.retrieveItemDataFromRmsWebpages(R"(C:\Users\detectivemark7\Desktop\RO\RMS\ItemDatabaseTraversal\)");
+
+    EXPECT_EQ(6225U, ragnarokOnline.getItemIdToItemMap().size());
+}
+
+TEST(RagnarokOnlineTest, RetrieveItemDataFromRmsWebPageWorksWithExample1)
+{
+    RagnarokOnline ragnarokOnline;
     ragnarokOnline.retrieveItemDataFromRmsWebPage(R"(C:\Users\detectivemark7\Desktop\RO\RMS\ItemDatabaseTraversal\itemWithLetter_a_pageNumber_1.html)");
 
     ItemIdToItemMap const& itemIdToItemMap(ragnarokOnline.getItemIdToItemMap());
