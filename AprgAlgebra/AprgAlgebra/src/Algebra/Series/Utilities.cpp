57e1e2bf09f5e897af4b559688cd291d92468f72
mevalba <markearvinalba@gmail.com>
2021-02-11 18:00:04 +0800

    "transferring series to aprgalgebra"

--- AprgAlgebra/AprgAlgebra/src/Algebra/Series/Utilities.cpp
+++ AprgAlgebra/AprgAlgebra/src/Algebra/Series/Utilities.cpp
@@ -1,10 +1,12 @@
 #include "Utilities.hpp"
 
 #include <Algebra/Term/Operators/TermOperators.hpp>
+#include <Math/AlbaMathHelper.hpp>
+
+using namespace alba::mathHelper;
 
 namespace alba
 {
-
 namespace algebra
 {
 
@@ -14,6 +16,34 @@ Term getSumOfArithmeticSeriesUsingFirstAndLastTerm(
         Term const& count)
 {
     return (firstTerm + lastTerm) * count / 2;
+}
+
+double getSumOfArithmeticSeriesUsingFirstAndLastValue(
+        double const firstValue,
+        double const lastValue,
+        int const count)
+{
+    return (firstValue + lastValue) * count / 2;
+}
+
+double getSumOfGeometricSeriesUsingFirstValueAndCommonMultiplier(
+        double const firstValue,
+        double const commonMultiplier,
+        int const count)
+{
+    return firstValue*(1-pow(commonMultiplier, count))/(1-commonMultiplier);
+}
+
+double getInfiniteSumOfGeometricSeriesIfCommonMultiplierIsFractional(
+        double const firstValue,
+        double const commonMultiplier)
+{
+    double result(0);
+    if(getAbsoluteValue(commonMultiplier) < 1)
+    {
+        result = firstValue/(1-commonMultiplier);
+    }
+    return result;
 }
 
 }
