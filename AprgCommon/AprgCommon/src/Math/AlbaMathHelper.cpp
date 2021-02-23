2e4e1dc395c2cc2e0ffa0a86f53b9cf1299e35e3
mevalba <markearvinalba@gmail.com>
2021-02-23 08:16:34 +0800

    "aprgcommon fixes"

--- AprgCommon/AprgCommon/src/Math/AlbaMathHelper.cpp
+++ AprgCommon/AprgCommon/src/Math/AlbaMathHelper.cpp
@@ -444,17 +444,17 @@ int getStirlingNumberOfTheSecondKind(unsigned int const n, unsigned int const k)
 {
     //In mathematics, particularly in combinatorics, a Stirling number of the second kind (or Stirling partition number) is the number of ways to partition a set of n objects into k non-empty subsets
     // Stirling numbers of the second kind occur in the field of mathematics called combinatorics and the study of partitions.
-    int sum=0;
+    AlbaNumber sum(0);
     for(unsigned int i=0; i<=k; i++)
     {
         int sign = isDivisible(i, 2) ? 1 : -1;
-        sum += sign * getNumberOfCombinations(k, i) * static_cast<unsigned int>(pow(k-i, n));
+        sum += AlbaNumber(sign) * getNumberOfCombinations(k, i) * pow(k-i, n);
     }
-    return sum / getFactorial(k);
+    sum /= getFactorial(k);
+    return static_cast<int>(sum.getInteger());
 }
 
-double getCumulativeStandardDistributionApproximation(double const z)
-{
+double getCumulativeStandardDistributionApproximation(double const z){
     return 0.5 * erfc(-z * pow(0.5, 0.5));
 }
 
