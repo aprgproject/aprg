8d9e6b42e16ef26ec415bfd1093b929fa9d8a2ee
mevalba <markearvinalba@gmail.com>
2021-02-26 09:48:10 +0800

    "added aprggeometry functionalities"

--- AprgGeometry/AprgGeometry/src/ThreeDimensions/EllipticHyperboloid.hpp
+++ AprgGeometry/AprgGeometry/src/ThreeDimensions/EllipticHyperboloid.hpp
@@ -13,11 +13,10 @@ class EllipticHyperboloid
 public:
     using TraverseOperation = std::function<void(Point const&)>;
     EllipticHyperboloid();
-    EllipticHyperboloid(Point const& center, double const aCoefficient, double const bCoefficient, double const cCoefficient, bool const signOfOne);
+    EllipticHyperboloid(Point const& center, double const aCoefficient, double const bCoefficient, double const cCoefficient, bool const isOneNegative);
     // ((x-center.x)^2/aCoefficient^2) + ((y-center.y)^2/bCoefficient^2) - ((z-center.z)^2/cCoefficient^2) = 1
     bool operator==(EllipticHyperboloid const& ellipsoid) const;
-    bool operator!=(EllipticHyperboloid const& ellipsoid) const;
-    Point getCenter() const;
+    bool operator!=(EllipticHyperboloid const& ellipsoid) const;    Point getCenter() const;
     double getAValue() const;
     double getBValue() const;
     double getCValue() const;
