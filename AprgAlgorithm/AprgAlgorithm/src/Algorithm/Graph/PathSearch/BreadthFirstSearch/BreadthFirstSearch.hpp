cc293c8f1c02c904b65e538254f2e3d17dcd611b
mevalba <markearvinalba@gmail.com>
2021-03-21 19:38:12 +0800

    "added aprgalgorithm functionalities"

--- AprgAlgorithm/AprgAlgorithm/src/Algorithm/Graph/PathSearch/BreadthFirstSearch/BreadthFirstSearch.hpp
+++ AprgAlgorithm/AprgAlgorithm/src/Algorithm/Graph/PathSearch/BreadthFirstSearch/BreadthFirstSearch.hpp
@@ -1,9 +1,6 @@
 #pragma once
 
-#include <Algorithm/Graph/UndirectedGraph/BaseUndirectedGraph.hpp>
-
-#include <functional>
-#include <map>
+#include <Algorithm/Graph/PathSearch/BasePathSearch.hpp>
 
 namespace alba
 {
@@ -11,24 +8,15 @@ namespace alba
 namespace algorithm
 {
 
-class BreadthFirstSearch
+class BreadthFirstSearch : public BasePathSearch
 {
 public:
-    using TraverseFunction=std::function<void(Path const& path, bool & shouldStop)>;
 
     BreadthFirstSearch(BaseUndirectedGraph const& graph, Vertex const startVertex);
 
-    bool hasPathTo(Vertex const endVertex) const;
     Path getShortestPathTo(Vertex const endVertex) const;
 
-    void reinitializeStartingFrom(Vertex const startVertex);
-
-private:
-    void clear();
-    BaseUndirectedGraph const& m_graph;
-    Vertex m_startVertex;
-    std::map<Vertex, Vertex> m_vertexToNextVertex;
-    std::map<Vertex, bool> m_isProcessed;
+    void reinitializeStartingFrom(Vertex const startVertex) override;
 };
 
 }
