ecc2396612e5007614cf70c70308b5649d5aba15
mevalba <markearvinalba@gmail.com>
2021-03-28 19:50:34 +0800

    "added aprgalgorithm functionalities"

--- AprgAlgorithm/AprgAlgorithm/src/Algorithm/Graph/PathSearch/Common/BasePathSearchForDijkstraAndDag.hpp
+++ AprgAlgorithm/AprgAlgorithm/src/Algorithm/Graph/PathSearch/Common/BasePathSearchForDijkstraAndDag.hpp
@@ -2,9 +2,10 @@
 
 #include <Algorithm/Graph/Types/GraphTypes.hpp>
 
+#include <functional>
+
 namespace alba
 {
-
 namespace algorithm
 {
 
@@ -15,12 +16,11 @@ public:
     using Graph = EdgeWeightedGraph;
     using Path = typename GraphTypes<Vertex>::Path;
     using EdgeWithWeight = typename GraphTypesWithWeights<Vertex, Weight>::EdgeWithWeight;
-    using SetOfVerticesWithWeight = typename GraphTypesWithWeights<Vertex, Weight>::SetOfVerticesWithWeight;
     using VertexToEdgeWithWeight = std::map<Vertex, EdgeWithWeight>;
+    using AdditionalRelaxationSteps = std::function<void(Vertex const&, Vertex const&, Weight const&)>;
 
     BasePathSearchForDijkstraAndDag(EdgeWeightedGraph const& graph, Vertex const& startVertex)
-        : m_graph(graph)
-        , m_startVertex(startVertex)
+        : m_graph(graph)        , m_startVertex(startVertex)
     {}
 
     bool hasPathTo(Vertex const& endVertex) const
@@ -75,11 +75,12 @@ protected:
         return result;
     }
 
-    void relaxAt(Vertex const& vertex)
+    void relaxAt(
+            Vertex const& vertex,
+            AdditionalRelaxationSteps const& additionalRelaxationSteps = [](Vertex const&, Vertex const&, Weight const&){})
     {
         for(Vertex const& adjacentVertex : m_graph.getAdjacentVerticesAt(vertex))
-        {
-            Weight weightOfCurrentEdge(m_graph.getWeight(vertex, adjacentVertex));
+        {            Weight weightOfCurrentEdge(m_graph.getWeight(vertex, adjacentVertex));
             Weight currentLowestWeightAtVertex(getSavedWeightAt(vertex));
             Weight currentWeightAtAdjacentVertex(getSavedWeightAt(adjacentVertex));
             if(hasNoWeightSaved(adjacentVertex)
@@ -87,18 +88,15 @@ protected:
             {
                 Weight newLowestWeight(currentLowestWeightAtVertex + weightOfCurrentEdge);
                 m_vertexToEdgeWithLowestWeight[adjacentVertex] = EdgeWithWeight(vertex, adjacentVertex, newLowestWeight);
-                m_foundVerticesOrderedByWeight.emplace(adjacentVertex, newLowestWeight);
+                additionalRelaxationSteps(vertex, adjacentVertex, newLowestWeight);
             }
         }
     }
-
     Graph const& m_graph;
     Vertex m_startVertex;
     VertexToEdgeWithWeight m_vertexToEdgeWithLowestWeight;
-    SetOfVerticesWithWeight m_foundVerticesOrderedByWeight;
 
 };
-
 }
 
 }
