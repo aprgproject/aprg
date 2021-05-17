4bf76a5cdf00f6645c9a6a4438c575c871880c20
mevalba <markearvinalba@gmail.com>
2021-05-17 23:33:11 +0800

    "aprgbitmap refactoring"

--- AprgBitmap/AprgBitmap/src/Bitmap/BitmapSnippet.cpp
+++ AprgBitmap/AprgBitmap/src/Bitmap/BitmapSnippet.cpp
@@ -240,11 +240,10 @@ void BitmapSnippet::setPixelAtForPixelInAByte(uint8_t * writer, unsigned int con
         uint32_t oldValue = static_cast<uint32_t>(*(writer+index));
         unsigned int shiftValue = calculateShiftValue(position);
         uint32_t replacePart = (m_configuration.getBitMaskForValue() & value) << shiftValue;
-        uint32_t retainMask = (m_configuration.getBitMaskForValue() << shiftValue) ^ AlbaBitValueUtilities<uint32_t>::getAllBitsAsserted();
+        uint32_t retainMask = (m_configuration.getBitMaskForValue() << shiftValue) ^ AlbaBitValueUtilities<uint32_t>::getAllOnes();
         uint32_t retainPart = (retainMask & oldValue);
         *(writer+index) = replacePart | retainPart;
-    }
-}
+    }}
 
 void BitmapSnippet::setPixelAtForMultipleBytePixels(uint8_t* writer, unsigned int const index, uint32_t const value)
 {
