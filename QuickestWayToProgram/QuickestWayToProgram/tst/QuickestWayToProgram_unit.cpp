0b01018d1e0848b8d280da7f7b8a55806e06f404
mevalba <markearvinalba@gmail.com>
2021-05-27 21:26:34 +0800

    "added QuickestWayToProgram test"

--- QuickestWayToProgram/QuickestWayToProgram/tst/QuickestWayToProgram_unit.cpp
+++ QuickestWayToProgram/QuickestWayToProgram/tst/QuickestWayToProgram_unit.cpp
@@ -11,10 +11,10 @@
 #include <windows.h>
 
 #include <algorithm>
+#include <bitset>
 #include <cstdio>
 #include <iostream>
-#include <iterator>
-#include <map>
+#include <iterator>#include <map>
 #include <string>
 
 using namespace std;
@@ -28,10 +28,28 @@ int numberOfFilesToBeAnalyzedForExtraction;
 int numberOfFilesAnalyzedForExtraction;
 }
 
+TEST(SampleTest, KBitAndNegativeK)
+{
+    /*int k=10;
+    int n=20;
+    while(k <= n)
+    {
+        ALBA_PRINT2(k, k&-k);
+        k += k&-k;
+    }*/
+
+    for(unsigned int k=0; k<=256; k++)
+    {
+        unsigned int p = k&-k;
+        ALBA_PRINT3(k, k-p+1, k);
+    }
+}
+
+/*
+
 TEST(SampleTest, RenameFiles)
 {
-    AlbaLocalPathHandler::ListOfPaths files;
-    AlbaLocalPathHandler::ListOfPaths directories;
+    AlbaLocalPathHandler::ListOfPaths files;    AlbaLocalPathHandler::ListOfPaths directories;
     AlbaLocalPathHandler pathHandler(R"(N:\MUSIC\111_DoAutomationHere)");
     pathHandler.findFilesAndDirectoriesUnlimitedDepth("*.*", files, directories);
 
@@ -43,12 +61,9 @@ TEST(SampleTest, RenameFiles)
     }
 }
 
-/*
-
 TEST(SampleTest, FindSourceFilesToAdjust_FileList)
 {
-    AlbaLocalPathHandler::ListOfPaths files;
-    AlbaLocalPathHandler::ListOfPaths directories;
+    AlbaLocalPathHandler::ListOfPaths files;    AlbaLocalPathHandler::ListOfPaths directories;
     AlbaLocalPathHandler pathHandler(R"(C:\Branches\CP\trunk\SBTS\tcom\C_Application\SC_TCOM)");
     pathHandler.findFilesAndDirectoriesUnlimitedDepth("*.*", files, directories);
 
