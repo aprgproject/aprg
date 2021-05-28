c815eeec574ccf5f641e15f0260ca62b95feca04
mevalba <markearvinalba@gmail.com>
2021-05-28 14:36:24 +0800

    "added aprgalgebra functionalities"

--- AprgAlgebra/AprgAlgebra/src/Algebra/Summation/Summation.cpp
+++ AprgAlgebra/AprgAlgebra/src/Algebra/Summation/Summation.cpp
@@ -120,25 +120,19 @@ void Summation::calculateSumUsingEachTerm(
     long long start(startNumber.getInteger());
     long long end(endNumber.getInteger());
     Term sum;
-    bool isFirst(true);
-    SubstitutionOfVariablesToValues substitution;
-    for(long long i(start); i<=end; i++)
+    if(start<=end)
     {
-        substitution.putVariableWithValue(m_variableToSubstitute, i);
-        Term termWithValue(substitution.performSubstitutionTo(m_termToSum));
-        if(isFirst)
+        SubstitutionOfVariablesToValues substitution;
+        substitution.putVariableWithValue(m_variableToSubstitute, start);
+        sum = substitution.performSubstitutionTo(m_termToSum);
+        for(long long value=start+1; value<=end; value++)
         {
-            sum = termWithValue;
-            isFirst=false;
-        }
-        else
-        {
-            sum = sum + termWithValue;
+            substitution.putVariableWithValue(m_variableToSubstitute, value);
+            sum += substitution.performSubstitutionTo(m_termToSum);
         }
     }
     result = sum;
 }
-
 void Summation::calculateSumUsingModel(
         Term & result,
         AlbaNumber const& startNumber,
