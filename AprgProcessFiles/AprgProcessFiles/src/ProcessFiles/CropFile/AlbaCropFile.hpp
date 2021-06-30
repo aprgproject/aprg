46e49ce8c5004f77575123c1de6cddb5d9f1c88a
mevalba <markearvinalba@gmail.com>
2021-06-30 22:47:34 +0800

    "aprgprocessfiles refactoring"

--- AprgProcessFiles/AprgProcessFiles/src/ProcessFiles/CropFile/AlbaCropFile.hpp
+++ AprgProcessFiles/AprgProcessFiles/src/ProcessFiles/CropFile/AlbaCropFile.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
-#include <Common/Optional/AlbaOptional.hpp>
+#include <Common/Container/AlbaOptional.hpp>
 #include <GrepStringEvaluator/AlbaGrepStringEvaluator.hpp>
 
-#include <functional>
-#include <string>
+#include <functional>#include <string>
 
 namespace alba
 {
