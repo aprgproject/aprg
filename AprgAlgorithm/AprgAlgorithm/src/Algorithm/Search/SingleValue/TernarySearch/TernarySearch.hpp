f49b38b7fd2bfbe9bee436d850fe142c74f5491d
mevalba <markearvinalba@gmail.com>
2021-07-20 17:06:04 +0800

    "adding aprgalgorithm functionalities"

--- AprgAlgorithm/AprgAlgorithm/src/Algorithm/Search/SingleValue/TernarySearch/TernarySearch.hpp
+++ AprgAlgorithm/AprgAlgorithm/src/Algorithm/Search/SingleValue/TernarySearch/TernarySearch.hpp
@@ -9,14 +9,14 @@ namespace algorithm
 {
 
 template <typename Values>
-class BinarySearchWithRecursion
+class TernarySearch
 {
 public:
     using Index = unsigned int;
     using Value = typename Values::value_type;
     static constexpr Index INVALID_INDEX = std::numeric_limits<Index>::max();
 
-    BinarySearchWithRecursion(Values const& sortedValues)
+    TernarySearch(Values const& sortedValues)
         : m_sortedValues(sortedValues)
     {}
 
@@ -71,22 +71,34 @@ private:
         }
         else
         {
-            Index middleIndex = (lowerIndex+higherIndex)/2;
-            Value middleValue(m_sortedValues.at(middleIndex));
-            if(value == middleValue)
+            Index oneThirdSize = (higherIndex-lowerIndex)/3;
+            Index firstMiddleIndex = lowerIndex + oneThirdSize;
+            Index secondMiddleIndex = firstMiddleIndex + oneThirdSize;
+            Value firstMiddleValue(m_sortedValues.at(firstMiddleIndex));
+            Value secondMiddleValue(m_sortedValues.at(secondMiddleIndex));
+            if(value == firstMiddleValue)
             {
-                result = middleIndex;
+                result = firstMiddleIndex;
             }
-            else if(value > middleValue)
+            else if(value == secondMiddleValue)
             {
-                result = getIndexOfNearestValueWithoutCheck(middleIndex+1, higherIndex, value);
+                result = secondMiddleIndex;
             }
-            else if(value < middleValue)
+            else if(firstMiddleValue > value) // if on the first one-third part
             {
-                result = getIndexOfNearestValueWithoutCheck(lowerIndex, middleIndex-1, value);
+                result = getIndexOfNearestValueWithoutCheck(lowerIndex, firstMiddleIndex-1, value);
+            }
+            else if(secondMiddleValue < value) // if on the third one-third part
+            {
+                result = getIndexOfNearestValueWithoutCheck(secondMiddleIndex+1, higherIndex, value);
+            }
+            else // if on the second one-third part
+            {
+                result = getIndexOfNearestValueWithoutCheck(firstMiddleIndex+1, secondMiddleIndex-1, value);
             }
         }
-        return result;    }
+        return result;
+    }
 
     Index getIndexOfNearestValueInBetweenTwoIndices(Index const lowerIndex, Index const higherIndex, Value const& value) const
     {
@@ -102,33 +114,21 @@ private:
 
 }
 
-// Binary Search: Search a sorted array by repeatedly dividing the search interval in half.
-// Begin with an interval covering the whole array.
-// If the value of the search key is less than the item in the middle of the interval, narrow the interval to the lower half.
-// Otherwise, narrow it to the upper half. Repeatedly check until the value is found or the interval is empty.
+// Which of the above two does less comparisons in worst case?
+// From the first look, it seems the ternary search does less number of comparisons as it makes Log3n recursive calls,
+// but binary search makes Log2n recursive calls.
 
-// We basically ignore half of the elements just after one comparison.
-// Compare x with the middle element.
-// If x matches with the middle element, we return the mid index.
-// Else If x is greater than the mid element, then x can only lie in the right half subarray after the mid element. So we recur for the right half.
-// Else (x is smaller) recur for the left half.
+// The following is recursive formula for counting comparisons in worst case of Binary Search.
+// -> T(n) = T(n/2) + 2,  T(1) = 1
 
-// Time Complexity:
-// The time complexity of Binary Search can be written as
-// T(n) = T(n/2) + c
-// The above recurrence can be solved either using the Recurrence Tree method or Master method.
-// It falls in case II of the Master Method and the solution of the recurrence is Theta(Logn) .
-// Auxiliary Space: O(1) in case of iterative implementation. In the case of recursive implementation, O(Logn) recursion call stack space.
+// The following is recursive formula for counting comparisons in worst case of Ternary Search.
+// -> T(n) = T(n/3) + 4, T(1) = 1
 
+// In binary search, there are 2Log2n + 1 comparisons in worst case. In ternary search, there are 4Log3n + 1 comparisons in worst case.
+// -> Time Complexity for Binary search = 2clog2n + O(1)
+// -> Time Complexity for Ternary search = 4clog3n + O(1)
 
-// Note, its better to have:
-// -> int mid = low + (high – low)/2;
-// Maybe, you wonder why we are calculating the middle index this way, we can simply add the lower and higher index and divide it by 2.
-// -> int mid = (low + high)/2;
-// But if we calculate the middle index like this means our code is not 100% correct, it contains bugs.
-// That is, it fails for larger values of int variables low and high.
-// Specifically, it fails if the sum of low and high is greater than the maximum positive int value(231 – 1).
-// The sum overflows to a negative value and the value stays negative when divided by 2. In java, it throws ArrayIndexOutOfBoundException.
-// -> int mid = low + (high – low)/2;
-// So it’s better to use it like this. This bug applies equally to merge sort and other divide and conquer algorithms.
+// Therefore, the comparison of Ternary and Binary Searches boils down the comparison of expressions 2Log3n and Log2n .
+// The value of 2Log3n can be written as (2 / Log2(3)) * Log2n .
+// Since the value of (2 / Log23) is more than one, Ternary Search does more comparisons than Binary Search in worst case.
 
