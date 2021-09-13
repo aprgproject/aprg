709e796dc94ed415b205ebd448388e961fe3419d
mevalba <markearvinalba@gmail.com>
2021-09-13 17:10:35 +0800

    aprgstatistics refactoring

--- AprgStatistics/AprgStatistics/tst/StatisticsTests/TwoDimensionsStatistics_unit.cpp
+++ AprgStatistics/AprgStatistics/tst/StatisticsTests/TwoDimensionsStatistics_unit.cpp
@@ -148,11 +148,10 @@ TEST(TwoDimensionsStatisticsTest, LineModelingWithScatteredValue)
 
     EXPECT_DOUBLE_EQ(1, lineModel.aCoefficient);
     EXPECT_DOUBLE_EQ(-0.016144305895888833025, lineModel.bCoefficient);
-    EXPECT_DOUBLE_EQ(-0.64846044526167945, lineModel.cCoefficient);
+    EXPECT_DOUBLE_EQ(-0.64846044526168001, lineModel.cCoefficient);
 }
 
-TEST(TwoDimensionsStatisticsTest, SquareErrorFromLineModelCanBeCalculatedForInvalidLine)
-{
+TEST(TwoDimensionsStatisticsTest, SquareErrorFromLineModelCanBeCalculatedForInvalidLine){
     LocalLineModel lineModel(0, 0, 0);
 
     EXPECT_EQ(0, TwoDimensionsStatistics::calculateSquareError(LocalSample{5, 3}, lineModel));
