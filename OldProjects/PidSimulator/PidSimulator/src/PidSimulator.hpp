6a5b71632cdca5d9a539455d8d837b15824c184b
mevalba <markearvinalba@gmail.com>
2021-09-20 00:36:00 +0800

    pidsimulator refactoring

--- OldProjects/PidSimulator/PidSimulator/src/PidSimulator.hpp
+++ OldProjects/PidSimulator/PidSimulator/src/PidSimulator.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
-#include <Common/Randomizer/AlbaRandomizer.hpp>
+#include <Common/Randomizer/AlbaSimpleRandomizer.hpp>
 #include <PidSimulatorConfiguration.hpp>
 
 #include <vector>
-
 namespace alba
 {
 
@@ -54,7 +53,7 @@ private:
     double m_xGridInterval;
     double m_yGridInterval;
     std::vector<double> m_inputSample;
-    AlbaRandomizer m_randomizer;
+    AlbaSimpleRandomizer m_randomizer;
 };
 
 }
