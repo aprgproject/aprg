94b7f9e67cddf36f98addbcafc7dc81f2dfb7eec
mevalba <markearvinalba@gmail.com>
2021-09-20 00:25:25 +0800

    aprggnuplot refactoring

--- AprgGnuPlot/AprgGnuPlot/src/AprgGnuPlot/AprgGnuPlotBase.cpp
+++ AprgGnuPlot/AprgGnuPlot/src/AprgGnuPlot/AprgGnuPlotBase.cpp
@@ -7,29 +7,28 @@ namespace alba
 
 void AprgGnuPlotBase::setTitle(string const& title, string const& configurationString)
 {
-    m_gnuPlot << R"(set title ")" << title << " " << configurationString << endl;
+    m_gnuPlot << R"(set title ")" << title << " " << configurationString << "\n";
 }
 
 void AprgGnuPlotBase::setXAxis(string const& xAxisName, string const& configurationString)
 {
-    m_gnuPlot << R"(set xlabel ")" << xAxisName << " " << configurationString << endl;
-    m_gnuPlot << R"(set xtics font ",15")" << endl;
+    m_gnuPlot << R"(set xlabel ")" << xAxisName << " " << configurationString << "\n";
+    m_gnuPlot << R"(set xtics font ",15")" << "\n";
 }
 
 void AprgGnuPlotBase::setYAxis(string const& yAxisName, string const& configurationString)
 {
-    m_gnuPlot << R"(set ylabel ")" << yAxisName << " " << configurationString << endl;
-    m_gnuPlot << R"(set ytics font ",15")" << endl;
+    m_gnuPlot << R"(set ylabel ")" << yAxisName << " " << configurationString << "\n";
+    m_gnuPlot << R"(set ytics font ",15")" << "\n";
 }
 
 void AprgGnuPlotBase::setZAxis(string const& zAxisName, string const& configurationString)
 {
-    m_gnuPlot << R"(set zlabel ")" << zAxisName << " " << configurationString << endl;
-    m_gnuPlot << R"(set ztics font ",15")" << endl;
+    m_gnuPlot << R"(set zlabel ")" << zAxisName << " " << configurationString << "\n";
+    m_gnuPlot << R"(set ztics font ",15")" << "\n";
 }
 
-void AprgGnuPlotBase::doGraphs(GraphingSteps const& graphingSteps)
-{
+void AprgGnuPlotBase::doGraphs(GraphingSteps const& graphingSteps){
     startGraph();
     graphingSteps();
     endGraph();
@@ -37,7 +36,7 @@ void AprgGnuPlotBase::doGraphs(GraphingSteps const& graphingSteps)
 
 void AprgGnuPlotBase::endGraph()
 {
-    m_gnuPlot << endl;
+    m_gnuPlot << "\n";
 }
 
 }
