1f4fd2abfb6e68ff57cafcf2d148fa87ec07ed83
mevalba <markearvinalba@gmail.com>
2021-09-20 00:36:20 +0800

    rttanalyzer refactoring

--- OldProjects/RttAnalyzer/RttAnalyzer/src/RttAnalyzer2.cpp
+++ OldProjects/RttAnalyzer/RttAnalyzer/src/RttAnalyzer2.cpp
@@ -42,16 +42,15 @@ void RttAnalyzer2::processLine(ofstream& outputFile, string const& line)
     static string dateTime;
     string time = getStringInBetweenTwoStrings(line, "<", "Z>");
     unsigned int rtt = convertStringToNumber<unsigned int>(getStringAfterThisString(line, "final RTT value reported to DSP explorer = "));
-    outputFile << time << "," << rtt << endl;
+    outputFile << time << "," << rtt << "\n";
 }
 
 void RttAnalyzer2::saveTitle2()
 {
-    m_outputLogStream  << "fileName,dateTime,maxPos,refPos,difference" << endl;
+    m_outputLogStream  << "fileName,dateTime,maxPos,refPos,difference\n";
 }
 
-void RttAnalyzer2::processFile2(string const& file)
-{
+void RttAnalyzer2::processFile2(string const& file){
     AlbaLocalPathHandler pathHandler(file);
     ifstream logStream(pathHandler.getFullPath());
 
@@ -79,10 +78,9 @@ void RttAnalyzer2::processLine2(string const& fileName, string const& line)
         unsigned int maxPos = convertStringToNumber<unsigned int>(getNumberAfterThisString(line, "max_pos[0]: "));
         unsigned int refPos = convertStringToNumber<unsigned int>(getNumberAfterThisString(line, "ref_pos: "));
         int difference = static_cast<int>(maxPos)-static_cast<int>(refPos);
-        m_outputLogStream << fileName <<"," << dateTime << ","<< maxPos << "," << refPos << "," << difference << endl;
+        m_outputLogStream << fileName <<"," << dateTime << ","<< maxPos << "," << refPos << "," << difference << "\n";
     }
 }
-
 void RttAnalyzer2::processFile3(string const& file)
 {
     AlbaLocalPathHandler pathHandler(file);
@@ -91,11 +89,10 @@ void RttAnalyzer2::processFile3(string const& file)
     pathHandler.input(pathHandler.getDirectory()+pathHandler.getFilenameOnly()+".csv");
     ofstream collectedRttDetails(pathHandler.getFullPath());
 
-    outputLogStream  << "peak_pos_cx8" << endl;
+    outputLogStream  << "peak_pos_cx8\n";
 
     if(logStream.is_open())
-    {
-        AlbaFileReader logFileReader(logStream);
+    {        AlbaFileReader logFileReader(logStream);
 
         while(logFileReader.isNotFinished())
         {
@@ -112,7 +109,7 @@ void RttAnalyzer2::processLine3(ofstream& outputFile, string const& line)
 {
     static string dateTime;
     unsigned int peakPosCx8 = convertStringToNumber<unsigned int>(getNumberAfterThisString(line, "peak_pos_cx8 "));
-    outputFile << peakPosCx8  << endl;
+    outputFile << peakPosCx8  << "\n";
 }
 
 }
