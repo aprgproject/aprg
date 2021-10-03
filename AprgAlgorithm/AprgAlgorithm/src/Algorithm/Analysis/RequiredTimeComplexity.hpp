f0d55c693805a1df7993f0d36d9bb19b573caa67
mevalba <markearvinalba@gmail.com>
2021-10-03 09:30:21 +0800

    aprgalgorithm refactoring

--- AprgAlgorithm/AprgAlgorithm/src/Algorithm/Analysis/RequiredTimeComplexity.hpp
+++ AprgAlgorithm/AprgAlgorithm/src/Algorithm/Analysis/RequiredTimeComplexity.hpp
@@ -18,9 +18,8 @@ namespace algorithm
 // | n<=20      | O(2^n)
 // | n<=500     | O(n^3)
 // | n<=5000    | O(n^2)
-// | n<=106     | O(nlogn) or O(n)
+// | n<=10^6    | O(nlogn) or O(n)
 // | n is large | O(1) or O(logn)
-
 
 // For example, if the input size is n = 10^5, it is probably expected that the time complexity of the algorithm is O(n) or O(nlogn).
 // This information makes it easier to design the algorithm, because it rules out approaches that would yield an algorithm with a worse time complexity.
