f9615573523e02c5b30cca9c053c52e126e9dba2
mevalba <markearvinalba@gmail.com>
2021-10-09 23:20:07 +0800

    added codingcompetitions functionalities

--- CodingCompetitions/GoogleKickStart/GoogleKickStart/tst/2019/RoundA/KickStart_2019_RoundA_P1_Training.cpp
+++ CodingCompetitions/GoogleKickStart/GoogleKickStart/tst/2019/RoundA/KickStart_2019_RoundA_P1_Training.cpp
@@ -7,11 +7,12 @@
 #endif
 // ~~~~~~~~~ DELETE THIS WHEN SUBMITTING END   ~~~~~~~~~
 
+#include <algorithm>
 #include <cstdint>
 #include <iostream>
+#include <vector>
 
 using namespace std;
-
 // ~~~~~~~~~ DELETE THIS WHEN SUBMITTING START ~~~~~~~~~
 #ifndef FOR_SUBMISSION
 using namespace alba;
@@ -27,11 +28,40 @@ namespace KickStart_2019_RoundA_P1_Training
 
 void runTestCase(unsigned int const testCaseNumber)
 {
-    //my_cout << "Case #" << testCaseNumber << ": " << answer << '\n';
+    int numberOfStudents, numberOfStudentsPerTeam;
+    my_cin >> numberOfStudents >> numberOfStudentsPerTeam;
+    vector<int> skillsOfStudents(numberOfStudents);
+    for(int i=0; i<numberOfStudents; ++i)
+    {
+        my_cin >> skillsOfStudents[i];
+    }
+
+    sort(skillsOfStudents.begin(), skillsOfStudents.end());
+
+    int hoursOfCoaching=0;
+    if(numberOfStudentsPerTeam <= numberOfStudents && numberOfStudents > 0 && numberOfStudentsPerTeam > 0)
+    {
+        int maxSkillOfTeam = *max_element(skillsOfStudents.cbegin(), skillsOfStudents.cbegin()+numberOfStudentsPerTeam);
+        int i=0;
+        for(; i<numberOfStudentsPerTeam; ++i)
+        {
+            hoursOfCoaching += maxSkillOfTeam-skillsOfStudents[i];
+        }
+
+        int currentHoursOfCoaching = hoursOfCoaching;
+        for(; i<numberOfStudents; ++i)
+        {
+            int maxDifference = skillsOfStudents[i] - skillsOfStudents[i-1];
+            int lastDifference = skillsOfStudents[i] - skillsOfStudents[i-numberOfStudentsPerTeam];
+            currentHoursOfCoaching = currentHoursOfCoaching + maxDifference*(numberOfStudentsPerTeam) - lastDifference;
+            hoursOfCoaching = min(hoursOfCoaching, currentHoursOfCoaching);
+        }
+    }
+
+    my_cout << "Case #" << testCaseNumber << ": " << hoursOfCoaching << '\n';
 }
 
-void runAllTestCases()
-{
+void runAllTestCases(){
     unsigned int numberOfTestCases;
     my_cin >> numberOfTestCases;
     for (unsigned int testCaseNumber = 1; testCaseNumber <= numberOfTestCases; testCaseNumber++)
