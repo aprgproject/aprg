9999b17098859723e4a30b5b9b338b5726f6d5de
mevalba <markearvinalba@gmail.com>
2022-03-27 03:24:30 +0800

    designpatterns refactoring

--- DesignPatterns/BasicExamples/src/Builder/Builder.hpp
+++ DesignPatterns/BasicExamples/src/Builder/Builder.hpp
@@ -83,11 +83,10 @@ private:
 
 }  // namespace Builder
 
-// Bridge discussion:
+// Builder discussion:
 
 // ONE LINE NOTE:
-// -> Create objects using an ABSTRACT/CHANGING BUILDER, building it STEP BY STEP, and DIRECTING it with CONSISTENT
-// build steps
+// -> Create objects using an ABSTRACT/CHANGING BUILDER, building it STEP BY STEP, and DIRECTING it with CONSISTENT// build steps
 
 // Intent:
 // Builder pattern has creational purpose and separates the construction of a complex object
