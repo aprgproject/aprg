fea9e6cd6d77021a3716123b0d51a7353f74f560
mevalba <markearvinalba@gmail.com>
2022-08-14 02:57:42 +0800

    chesspeek refactoring

--- Chess/ChessPeek/ChessPeek/src/ChessPeek/BoardObserver.hpp
+++ Chess/ChessPeek/ChessPeek/src/ChessPeek/BoardObserver.hpp
@@ -28,11 +28,10 @@ public:
         WhiteOrBlack condition;
     };
     using BitSet64 = std::bitset<64>;
-    using Count = unsigned int;
+    using Count = int;
     using PieceToChessCellBitValueMap = std::map<PieceColorAndType, uint64_t>;
     using BitValueUtilities = AlbaBitValueUtilities<uint64_t>;
-    using PieceColorAndTypes = std::vector<PieceColorAndType>;
-    using CheckDetails = std::vector<CheckDetail>;
+    using PieceColorAndTypes = std::vector<PieceColorAndType>;    using CheckDetails = std::vector<CheckDetail>;
     using BoolFunction = std::function<bool(double const)>;
 
     BoardObserver() = delete;
--- Chess/ChessPeek/ChessPeek/src/ChessPeek/Utilities.cpp
+++ Chess/ChessPeek/ChessPeek/src/ChessPeek/Utilities.cpp
@@ -11,17 +11,12 @@ namespace chess {
 
 namespace ChessPeek {
 
-XY convertToXY(BitmapXY const& bitmapXY) {
-    return {static_cast<int>(bitmapXY.getX()), static_cast<int>(bitmapXY.getY())};
-}
+XY convertToXY(BitmapXY const& bitmapXY) { return bitmapXY; }
 
-BitmapXY convertToBitmapXY(XY const& xy) {
-    return {static_cast<unsigned int>(xy.getX()), static_cast<unsigned int>(xy.getY())};
-}
+BitmapXY convertToBitmapXY(XY const& xy) { return xy; }
 
 double calculateColorIntensityDecimal(uint32_t const color) {
-    return (((double)extractRed(color) + extractGreen(color) + extractBlue(color)) / 0xFF) / 3;
-}
+    return (((double)extractRed(color) + extractGreen(color) + extractBlue(color)) / 0xFF) / 3;}
 
 uint8_t extractRed(uint32_t const color) { return (AlbaBitManipulation<uint32_t>::getByteAt<2>(color)); }
 
