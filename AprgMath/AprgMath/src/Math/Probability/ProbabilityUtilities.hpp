b8a26624205ef37a7ea5d14528815be1189c7253
mevalba <markearvinalba@gmail.com>
2022-08-29 07:43:01 +0800

    aprgmath refactoring

--- AprgMath/AprgMath/src/Math/Probability/ProbabilityUtilities.hpp
+++ AprgMath/AprgMath/src/Math/Probability/ProbabilityUtilities.hpp
@@ -20,13 +20,12 @@ AlbaNumber getCorrectProbability(AlbaNumber const& probability);
 
 AlbaNumber getProbability(UnsignedInteger const numberOfDesiredOutcome, UnsignedInteger const totalNumberOfOutcomes);
 AlbaNumber getProbabilityOnBinomialDistribution(
-    AlbaNumber const& probabilityOfASingleAttempt, UnsignedInteger const xTargetTries, UnsignedInteger const nTries);
+    AlbaNumber const& probabilityOfASingleAttempt, UnsignedInteger const xTarget, UnsignedInteger const nTries);
 AlbaNumber getProbabilityOnGeometricDistribution(
-    AlbaNumber const& probabilityOfASingleAttempt, UnsignedInteger const xTargetTries);
+    AlbaNumber const& probabilityOfASingleAttempt, UnsignedInteger const xTarget);
 
 AlbaNumber getComplementOfProbability(AlbaNumber const& probability);
-AlbaNumber getUnionOfProbabilitiesOfDisjointEvents(
-    AlbaNumber const& probabilityOfEventA, AlbaNumber const& probabilityOfEventB);
+AlbaNumber getUnionOfProbabilitiesOfDisjointEvents(    AlbaNumber const& probabilityOfEventA, AlbaNumber const& probabilityOfEventB);
 AlbaNumber getUnionOfProbabilities(
     AlbaNumber const& probabilityOfEventA, AlbaNumber const& probabilityOfEventB,
     AlbaNumber const& probabilityOfEventAAndEventB);
@@ -43,6 +42,8 @@ AlbaNumber getExpectedValueInUniformDistribution(AlbaNumber const& lowestValue, 
 AlbaNumber getExpectedValueInBinomialDistribution(
     AlbaNumber const& probabilityOfASingleAttempt, UnsignedInteger const nTries);
 AlbaNumber getExpectedValueInGeometricDistribution(AlbaNumber const& probability);
+
+AlbaNumber getNumberOfPeopleForTheBirthdayParadox(AlbaNumber const& propbabilityThatMustBeMet);
 
 }  // namespace math
 
