1bd77e61f4e7450cfb144ae4d44e90988b2e537e
mevalba <markearvinalba@gmail.com>
2023-03-21 21:59:37 +0800

    added test for quickestwaytoprogram

--- QuickestWayToProgram/QuickestWayToProgram/tst/QuickestWayToProgram_unit.cpp
+++ QuickestWayToProgram/QuickestWayToProgram/tst/QuickestWayToProgram_unit.cpp
@@ -29,10 +29,18 @@ int numberOfFilesToBeAnalyzedForExtraction;
 int numberOfFilesAnalyzedForExtraction;
 }  // namespace ProgressCounters
 
-bool canConstructVer2(string ransomNote, string magazine) {
+TEST(SampleTest, PrimerTest) {
+    int i;
+    double d;
+    d = i = 3.5;
+    ALBA_PRINT2(i, d);  // 3,3
+    i = d = 3.5;
+    ALBA_PRINT2(i, d);  // 3, 3.5
+}
+
+/*bool canConstructVer2(string ransomNote, string magazine) {
     array<int, 26> magazineLetterToCount{};
     auto itMagazine = magazine.begin();
-
     for (char const c : ransomNote) {
         int& count = magazineLetterToCount[c - 'a'];
         if (count > 0) {
@@ -178,11 +186,10 @@ TEST(SampleTest, KWeakestRows) {
     EXPECT_EQ(nullptr, result->next->next->next->next->next);
 }
 
-/*TEST(SampleTest, NumberOfSteps) {
+TEST(SampleTest, NumberOfSteps) {
     EXPECT_EQ(0, numberOfSteps(0));
     EXPECT_EQ(1, numberOfSteps(1));
-    EXPECT_EQ(2, numberOfSteps(2));
-    EXPECT_EQ(3, numberOfSteps(3));
+    EXPECT_EQ(2, numberOfSteps(2));    EXPECT_EQ(3, numberOfSteps(3));
     EXPECT_EQ(3, numberOfSteps(4));
     EXPECT_EQ(4, numberOfSteps(5));
     EXPECT_EQ(4, numberOfSteps(6));
