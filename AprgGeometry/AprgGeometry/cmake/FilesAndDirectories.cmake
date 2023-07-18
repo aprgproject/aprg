3f0a7169b2a5fc159d1004ac21ac7fe6fa366bbb
mevalba <markearvinalba@gmail.com>
2023-07-18 19:34:19 +0800

    aprggeometry refactoring and added json files

--- AprgGeometry/AprgGeometry/cmake/FilesAndDirectories.cmake
+++ AprgGeometry/AprgGeometry/cmake/FilesAndDirectories.cmake
@@ -1,10 +1,6 @@
-include(${APRG_DIR}/AprgCMakeHelpers/FileAndDirectoryMacros.cmake)
-include(${APRG_DIR}/AprgCMakeHelpers/PrintMacros.cmake)
-
 set(APRG_GEOMETRY_SOURCE_DIRECTORY
     ${APRG_GEOMETRY_DIR}/src
 )
-
 set(APRG_GEOMETRY_TEST_DIRECTORY
     ${APRG_GEOMETRY_DIR}/tst
 )
