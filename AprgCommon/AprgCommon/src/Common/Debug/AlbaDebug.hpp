7db5bf1b4a79cc8877a21f230b2400e100ec901e
mevalba <markearvinalba@gmail.com>
2023-07-20 04:39:36 +0800

    aprgcommon refactoring

--- AprgCommon/AprgCommon/src/Common/Debug/AlbaDebug.hpp
+++ AprgCommon/AprgCommon/src/Common/Debug/AlbaDebug.hpp
@@ -5,9 +5,9 @@
 
 #include <iomanip>
 #include <iostream>
+#include <string_view>
 
 namespace alba {
-
 #if defined(__clang__) || defined(__GNUC__) || defined(__GNUG__)
 #warning("The class AlbaDebug is used. Please remove after your done debugging.") // GCC extension
 #elif defined(_MSC_VER)
@@ -26,13 +26,18 @@ inline std::ofstream debugStream(ALBA_PRINT_EXTERNAL_OUTPUT_STREAM_FILE_PATH);
 
 // Internal macros
 // This is a revised approach in printing because of MSVC limitation on macros.
-#define Z_ALBA_PRINT_START                                                                              \
-    ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT in line:" << std::setw(4) << ALBA_MACROS_GET_LINE << " in " \
-                             << ALBA_MACROS_GET_FUNCTION << "(...): ";
+inline std::string_view getShortenFunctionName(std::string_view longFunctionName) {
+    constexpr decltype(longFunctionName.length()) minimumLength = 40;
+    auto length = longFunctionName.length();
+    auto shortenLength = (std::min)(length, minimumLength);
+    return longFunctionName.substr(length - shortenLength, shortenLength);
+}
+#define Z_ALBA_PRINT_START                                                                                 \
+    ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT in line:" << std::setw(4) << ALBA_MACROS_GET_LINE << " in ..." \
+                             << getShortenFunctionName(ALBA_MACROS_GET_FUNCTION) << "(...): ";
 #define Z_ALBA_PRINT_END ALBA_PRINT_OUTPUT_STREAM << std::endl
 #define Z_ALBA_PRINT_PARAMETER_ONLY(parameter) \
-    printParameterWithName(ALBA_PRINT_OUTPUT_STREAM, ALBA_MACROS_GET_STRING_LITERAL(parameter), parameter);
-#define Z_ALBA_PRINT_SEPARATOR ALBA_PRINT_OUTPUT_STREAM << " ";
+    printParameterWithName(ALBA_PRINT_OUTPUT_STREAM, ALBA_MACROS_GET_STRING_LITERAL(parameter), parameter);#define Z_ALBA_PRINT_SEPARATOR ALBA_PRINT_OUTPUT_STREAM << " ";
 #define Z_ALBA_PRINT_PARAMETER(parameter) Z_ALBA_PRINT_PARAMETER_ONLY(parameter) Z_ALBA_PRINT_SEPARATOR
 #define Z_ALBA_PRINT_7(p1, p2, p3, p4, p5, p6, p7) Z_ALBA_PRINT_PARAMETER(p1) Z_ALBA_PRINT_6(p2, p3, p4, p5, p6, p7)
 #define Z_ALBA_PRINT_6(p1, p2, p3, p4, p5, p6) Z_ALBA_PRINT_PARAMETER(p1) Z_ALBA_PRINT_5(p2, p3, p4, p5, p6)
