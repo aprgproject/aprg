6dbc7be1c276c176a9070941576cb7e9548a90e5
mevalba <markearvinalba@gmail.com>
2023-07-20 02:52:12 +0800

    heartee refactoring and added json files

--- OldProjects/HeartTee/HeartTee/cmake/FilesAndDirectories.cmake
+++ OldProjects/HeartTee/HeartTee/cmake/FilesAndDirectories.cmake
@@ -1,10 +1,6 @@
-include(${APRG_DIR}/AprgCMakeHelpers/FileAndDirectoryMacros.cmake)
-include(${APRG_DIR}/AprgCMakeHelpers/PrintMacros.cmake)
-
 set(HEART_TEE_SOURCE_DIRECTORY
     ${HEART_TEE_DIR}/src
 )
-
 set(HEART_TEE_TEST_DIRECTORY
     ${HEART_TEE_DIR}/tst
 )
