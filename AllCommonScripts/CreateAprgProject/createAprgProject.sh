e427d1f94303f3ee96b338c4431d1c35e5ba9820
mevalba <markearvinalba@gmail.com>
2023-08-11 13:48:19 +0800

    added shell script to create new projects from templates

---
+++ AllCommonScripts/CreateAprgProject/createAprgProject.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Set variable values
+scriptPath=$(realpath "$0")
+scriptDirectory=$(dirname "$scriptPath")
+scriptName=$(basename "$scriptPath")
+aprgDirectory=$(realpath "$scriptDirectory/../../")
+scriptOption=$1
+projectName=$2
+capitalUnderscoreName=$3
+parentDirectory=$(realpath "$4")
+
+# Source needed scripts
+source "$aprgDirectory/AllCommonScripts/PrintScripts/PrintUtilities.sh"
+
+# Validate input
+if [ -z "$scriptOption" ]; then
+    scriptPrint $scriptName $LINENO "Error: The scriptOption cannot be empty, scriptOption: [$scriptOption]."
+	exit 1
+fi
+if [ -z "$projectName" ]; then
+    scriptPrint $scriptName $LINENO "Error: The projectName cannot be empty, projectName: [$projectName]."
+	exit 1
+fi
+if ! [[ -d $parentDirectory ]]; then
+    scriptPrint $scriptName $LINENO "Error: The directory [$parentDirectory] does not exist."
+	exit 1
+fi
+
+# Utility Functions
+copyTemplateFiles() {
+    templateName="$1"
+	templateDirectory="$aprgDirectory/$templateName"
+	projectDirectory="$parentDirectory/$projectName"
+    scriptPrint $scriptName $LINENO "copyTemplateFiles() [$templateName] [$templateDirectory] [$projectDirectory]"
+	mkdir -p $projectDirectory
+    rsync --verbose --archive --recursive --force --include='*/' --include='.*' $templateDirectory/* $projectDirectory
+}
+
+replaceAllText() {
+    local targetString="$1"
+    local replacementString="$2"
+    scriptPrint $scriptName $LINENO "replaceAllText() [$targetString] [$replacementString]"
+	$aprgDirectory/AllCommonScripts/ReplaceTextScripts/replaceText.sh "$projectDirectory" $targetString $replacementString
+}
+
+if [ "$scriptOption" == "TemplateForAllCases" ]; then
+    copyTemplateFiles "TemplateForAllCases" 
+    cd $projectDirectory
+    replaceAllText "TemplateForAllCases" $projectName
+    replaceAllText "TEMPLATE_FOR_ALL_CASES" $capitalUnderscoreName
+elif [ "$scriptOption" == "TemplateForLibraries" ]; then
+    copyTemplateFiles "TemplateForLibraries" 
+    cd $projectDirectory
+    replaceAllText "TemplateForLibraries" $projectName
+    replaceAllText "TEMPLATE_FOR_LIBRARIES" $capitalUnderscoreName
+elif [ "$scriptOption" == "TemplateForPrograms" ]; then
+    copyTemplateFiles "TemplateForPrograms" 
+    cd $projectDirectory
+    replaceAllText "TemplateForPrograms" $projectName
+    replaceAllText "TEMPLATE_FOR_PROGRAMS" $capitalUnderscoreName
+else
+	scriptPrint $scriptName $LINENO "The script option [$scriptOption] is not supported by this script."
+	exitCode=1
+fi
---
+++ AllCommonScripts/ReplaceTextScripts/replaceText.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Set variable values
+scriptPath=$(realpath "$0")
+scriptDirectory=$(dirname "$scriptPath")
+scriptName=$(basename "$scriptPath")
+aprgDirectory=$(realpath "$scriptDirectory/../../")
+replaceDirectory=$(realpath "$1")
+targetString=$2
+replacementString=$3
+
+# Source needed scripts
+source "$aprgDirectory/AllCommonScripts/PrintScripts/PrintUtilities.sh"
+
+# Validate input
+if ! [[ -d $replaceDirectory ]]; then
+    scriptPrint $scriptName $LINENO "Error: The directory [$replaceDirectory] does not exist."
+	exit 1
+fi
+if [ -z "$targetString" ]; then
+    scriptPrint $scriptName $LINENO "Error: The targetString cannot be empty, targetString: [$targetString]."
+	exit 1
+fi
+
+scriptPrint $scriptName $LINENO "Replacing text with [$targetString] to [$replacementString] in [$replaceDirectory]."
+
+# Rename directories
+find . -depth -type d -name "*$targetString*" -execdir bash -c 'newname=$(echo "$1" | sed "s/$2/$3/g"); mv "$1" "$newname"' bash {} "$targetString" "$replacementString" \;
+
+# Rename filenames
+find . -depth -type f -name "*$targetString*" -execdir rename "s/$targetString/$replacementString/g" {} \;
+
+# Replace contents
+find . -type f -exec sed -i "s/$targetString/$replacementString/g" {} +
+
+scriptPrint $scriptName $LINENO "Replacements for contents completed."
