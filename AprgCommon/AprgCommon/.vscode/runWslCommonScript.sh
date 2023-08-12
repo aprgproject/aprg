ffde08301129f29c95aa773ab4be3e74cc16264a
mevalba <markearvinalba@gmail.com>
2023-08-13 00:19:54 +0800

    aprgcommon fixed for github workflow

--- AprgCommon/AprgCommon/.vscode/runWslCommonScript.sh
+++ AprgCommon/AprgCommon/.vscode/runWslCommonScript.sh
@@ -25,15 +25,15 @@ source "$aprgDirectory/AllCommonScripts/PrintScripts/PrintUtilities.sh"
 # Running WSL Build And Run Script
 if [ "$scriptRunningOption" == "outputWithHighlighting" ]; then
 	# This command prints output with highlighting because stdout and stderr are maintained
-	"$aprgDirectory/AllCommonScripts/WslScripts/BuildAndRunInWsl.sh" $2 $3 $4
+	"$aprgDirectory/AllCommonScripts/WslScripts/BuildAndRunInWsl.sh" $2 $3
 	exitCode=$?
 elif [ "$scriptRunningOption" == "outputWithAbsolutePaths" ]; then
     # This command prints output without highlighting but with corrected absolute paths
-	"$aprgDirectory/AllCommonScripts/WslScripts/BuildAndRunInWsl.sh" $2 $3 $4 2>&1 | sed -E "s|\/mnt\/(\w+)\/|\U\1:/|g"
+	"$aprgDirectory/AllCommonScripts/WslScripts/BuildAndRunInWsl.sh" $2 $3 2>&1 | sed -E "s|\/mnt\/(\w+)\/|\U\1:/|g"
 	exitCode=$?
 elif [ "$scriptRunningOption" == "outputWithRelativePaths" ]; then
     # This command prints output without highlighting but with corrected relative paths from the project directory
-	"$aprgDirectory/AllCommonScripts/WslScripts/BuildAndRunInWsl.sh" $2 $3 $4 2>&1 | sed -E "s|$(pwd)||g"
+	"$aprgDirectory/AllCommonScripts/WslScripts/BuildAndRunInWsl.sh" $2 $3 2>&1 | sed -E "s|$(pwd)||g"
 	exitCode=$?
 else
 	scriptPrint $scriptName $LINENO "The script option [$scriptOption] is not found."
