577b1d78367b463b735a8362dbaa116d29586ba1
mevalba <markearvinalba@gmail.com>
2023-08-12 19:42:22 +0800

    added more prints in the last step

--- AprgCMakeHelpers/LastStep.cmake
+++ AprgCMakeHelpers/LastStep.cmake
@@ -1,4 +1,6 @@
 #Print final variables
+message("Final CMAKE_C_COMPILER is: [${CMAKE_C_COMPILER}]")
+message("Final CMAKE_CXX_COMPILER is: [${CMAKE_CXX_COMPILER}]")
 message("Final CMAKE_C_FLAGS are: [${CMAKE_C_FLAGS}]")
 message("Final CMAKE_CXX_FLAGS are: [${CMAKE_CXX_FLAGS}]")
 message("Final CMAKE_EXE_LINKER_FLAGS are: [${CMAKE_EXE_LINKER_FLAGS}]")
