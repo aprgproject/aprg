938a6fcf4fd11ceb0eeec3b5cc7971afb87ceaec
mevalba <markearvinalba@gmail.com>
2023-08-13 00:21:06 +0800

    removed project directory as an argument in wsl script

--- AllCommonScripts/WslScripts/BuildAndRunInWsl.sh
+++ AllCommonScripts/WslScripts/BuildAndRunInWsl.sh
@@ -5,31 +5,27 @@ scriptPath=$(realpath "$0")
 scriptDirectory=$(dirname "$scriptPath")
 scriptName=$(basename "$scriptPath")
 aprgDirectory=$(realpath "$scriptDirectory/../../")
-WslBuildFolderName="WslBuild"
+immediateDirectoryName=$(basename "$(dirname "$(pwd)")")
+wslBuildDirectoryName="WslBuild"
 numberOfCoresInTheSystem=$(nproc)
 exitCode=0
-projectName=$1
-scriptOption=$2
-firstArgument=$3
+scriptOption=$1
+firstArgument=$2
 
 # Source needed scripts
 source "$aprgDirectory/AllCommonScripts/PrintScripts/PrintUtilities.sh"
 
 # Display variable values
-scriptPrint $scriptName $LINENO "The project name is [$projectName] and the build option is [$scriptOption]"
+scriptPrint $scriptName $LINENO "The build option is [$scriptOption]"
 scriptPrint $scriptName $LINENO "The script path is [$scriptPath]"
-scriptPrint $scriptName $LINENO "The WSL build folder name is [$WslBuildFolderName]"
+scriptPrint $scriptName $LINENO "The immediate directory name is [$immediateDirectoryName]"
+scriptPrint $scriptName $LINENO "The WSL build directory name is [$wslBuildDirectoryName]"
 scriptPrint $scriptName $LINENO "The first argument is [$firstArgument]"
 
 # Setup folders for compilation
-if [ "$projectName" != $(basename "$(pwd)") ]; then
-	scriptPrint $scriptName $LINENO "Error: The current path [$(pwd)] does not have the project name [$projectName] as its directory."
-	scriptPrint $scriptName $LINENO "Please run this at the project directory (where the codes are located)."
-	exit 1
-fi
 cd ..
-mkdir -p $WslBuildFolderName
-cd $WslBuildFolderName
+mkdir -p $wslBuildDirectoryName
+cd $wslBuildDirectoryName
 scriptPrint $scriptName $LINENO "The build path is [$(pwd)]"
 
 # Enable the "exit on error" option to automatically stop if there is a failure
@@ -37,25 +33,33 @@ set -e
 
 # Perform script actions
 if [ "$scriptOption" == "clean" ]; then
-	if [ "$WslBuildFolderName" = $(basename "$(pwd)") ]; then
+	if [ "$wslBuildDirectoryName" = $(basename "$(pwd)") ]; then
 		scriptPrint $scriptName $LINENO "Deleting everything in [$(pwd)]."
 		rm -rf *
 		exitCode=$?
 		scriptPrint $scriptName $LINENO "After deletion in [$(pwd)] contents: [$(ls)]"
 	fi
 elif [ "$scriptOption" == "buildWithGcc" ]; then
-	cmake -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ "../$projectName/"
+	cmake -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ "../$immediateDirectoryName/"
 	make -j$numberOfCoresInTheSystem
 	exitCode=$?
 elif [ "$scriptOption" == "buildWithClang" ]; then
-	cmake -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ "../$projectName/"
+	cmake -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ "../$immediateDirectoryName/"
 	make -j$numberOfCoresInTheSystem
 	exitCode=$?
 elif [ "$scriptOption" == "buildWithClangWithAsan" ]; then
-	cmake -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_C_FLAGS:STRING="${CMAKE_C_FLAGS} -g --coverage -fsanitize=address -fno-omit-frame-pointer" -DCMAKE_CXX_FLAGS:STRING="${CMAKE_C_FLAGS} -g --coverage -fsanitize=address -fno-omit-frame-pointer" "../$projectName/"
+	cmake -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_C_FLAGS:STRING="${CMAKE_C_FLAGS} -g --coverage -fsanitize=address -fno-omit-frame-pointer" -DCMAKE_CXX_FLAGS:STRING="${CMAKE_C_FLAGS} -g --coverage -fsanitize=address -fno-omit-frame-pointer" "../$immediateDirectoryName/"
 	make -j$numberOfCoresInTheSystem
 	exitCode=$?
-elif [ "$scriptOption" == "runFileWithProjectName" ]; then
+elif [ "$scriptOption" == "install" ]; then
+	make install
+	exitCode=$?
+elif [ "$scriptOption" == "run" ]; then
+    projectName=$(cmake -P $aprgDirectory/AprgCMakeHelpers/GetProjectName.cmake)
+    if [ -z "$projectName" ]; then
+        scriptPrint $scriptName $LINENO "Error: The projectName is not determined using cmake."
+	    exit 1
+    fi
 	scriptPrint $scriptName $LINENO "Running executable: [$(pwd)/$projectName]."
 	"./$projectName" "$firstArgument"
 	exitCode=$?
