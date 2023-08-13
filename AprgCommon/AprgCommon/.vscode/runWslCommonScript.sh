d51777fca2131b2050dffe3607944e950a8d9af4
mevalba <markearvinalba@gmail.com>
2023-08-13 20:29:15 +0800

    corrected cmake and scripts

--- AprgCommon/AprgCommon/.vscode/runWslCommonScript.sh
+++ AprgCommon/AprgCommon/.vscode/runWslCommonScript.sh
@@ -8,36 +8,46 @@ scriptRunningOption=$1
 locateScriptPath=$(realpath "$scriptDirectory/locateAprgDirectory.sh")
 exitCode=0
 
-# Source needed scripts
+# Find aprgDirectory
+echo "$scriptName:$LINENO: Finding aprg directory..."
 if ! [[ -e $locateScriptPath ]]; then
 	echo "$scriptName:$LINENO: Error: The script [$locateScriptPath] does not exist."
 	exit 1
 fi
 source "$scriptDirectory/locateAprgDirectory.sh"
 findAprgDirectory $scriptDirectory
-# Validate path
+
+# Validate aprgDirectory
 if ! [[ -e $aprgDirectory ]]; then
 	echo "$scriptName:$LINENO: Error: The script [$aprgDirectory] does not exist."
 	exit 1
 fi
+
+# Source needed scripts
 source "$aprgDirectory/AllCommonScripts/PrintScripts/PrintUtilities.sh"
 
 # Running WSL Build And Run Script
+buildAndRunScriptPath=$(realpath "$aprgDirectory/AllCommonScripts/BuildAndRunScripts/BuildAndRun.sh")
+if ! [[ -e $buildAndRunScriptPath ]]; then
+	scriptPrintInCppMatcherFormat $scriptPath $LINENO ${BASH_LINENO[0]} "error: The script [$buildAndRunScriptPath] does not exist."
+	exit 1
+fi
+
 if [ "$scriptRunningOption" == "outputWithHighlighting" ]; then
 	# This command prints output with highlighting because stdout and stderr are maintained
-	"$aprgDirectory/AllCommonScripts/WslScripts/BuildAndRunInWsl.sh" $2 $3
+	$buildAndRunScriptPath $2 $3 $4 $5
 	exitCode=$?
 elif [ "$scriptRunningOption" == "outputWithAbsolutePaths" ]; then
     # This command prints output without highlighting but with corrected absolute paths
-	"$aprgDirectory/AllCommonScripts/WslScripts/BuildAndRunInWsl.sh" $2 $3 2>&1 | sed -E "s|\/mnt\/(\w+)\/|\U\1:/|g"
+	$buildAndRunScriptPath $2 $3 $4 $5 2>&1 | sed -E "s|\/mnt\/(\w+)\/|\U\1:/|g"
 	exitCode=$?
 elif [ "$scriptRunningOption" == "outputWithRelativePaths" ]; then
     # This command prints output without highlighting but with corrected relative paths from the project directory
-	"$aprgDirectory/AllCommonScripts/WslScripts/BuildAndRunInWsl.sh" $2 $3 2>&1 | sed -E "s|$(pwd)||g"
+	$buildAndRunScriptPath $2 $3 $4 $5 2>&1 | sed -E "s|$(pwd)||g"
 	exitCode=$?
 else
 	scriptPrint $scriptName $LINENO "The script option [$scriptOption] is not found."
-	echo "$scriptPath:$LINENO:${BASH_LINENO[0]}: error: The script option [$scriptOption] is not supported by the WSL shell script."
+	scriptPrintInCppMatcherFormat $scriptPath $LINENO ${BASH_LINENO[0]} "error: The script option [$scriptOption] is not supported by the WSL shell script."
 	exitCode=$?
 fi
 
