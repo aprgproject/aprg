4bd86b5d863aaeaa3ab36c9b7340dfcfb1c8bdc5
mevalba <markearvinalba@gmail.com>
2023-08-14 23:12:45 +0800

    committed so it wont be lost again

--- AllCommonScripts/DetectChangedProjects/DetectChangedProjects.sh
+++ AllCommonScripts/DetectChangedProjects/DetectChangedProjects.sh
@@ -5,13 +5,33 @@ scriptPath=$(realpath "$0")
 scriptDirectory=$(dirname "$scriptPath")
 scriptName=$(basename "$scriptPath")
 aprgDirectory=$(realpath "$scriptDirectory/../../")
+aprgDirectoryName="aprg"
 
 # Source needed scripts
 source "$aprgDirectory/AllCommonScripts/PrintScripts/PrintUtilities.sh"
 
 # Detect git changes
-changedFiles=$(git diff --name-only HEAD^)
+#detectedFiles=$(git diff --name-only HEAD^)
 
+detectedProjects=""
+searchingDirectory="$aprgDirectory/AprgCommon/AprgCommon/src/Common/Container/AlbaXY.hpp"
+while [ "$searchingDirectory" != "/" ]; do
+    scriptPrint $scriptName $LINENO "The searchingDirectory is: [$searchingDirectory]"
+	if [ $(basename "$searchingDirectory") == "$aprgDirectoryName" ]; then
+		break
+    elif ! [[ -e $"$searchingDirectory/CMakeLists.txt" ]]; then
+        formattedRelativePath=$(echo "\"$searchingDirectory\"" || sed -E "s|$aprgDirectory|\|")
+    	if [[ -z $detectedProjects ]]; then
+            detectedProjects=$formattedRelativePath
+        else
+            detectedProjects=$detectedProjects,$formattedRelativePath
+        if
+	else
+		searchingDirectory=$(dirname "$searchingDirectory")
+	fi
+done
+
+scriptPrint $scriptName $LINENO "The detectedProjects is: [$detectedProjects]"
 # Output projects
-changedProjects="AprgCommon/AprgCommon,AprgMath/AprgMath"
-echo "::set-output name=ProjectsToBuild::$changedProjects"
+detectedProjects="\"AprgCommon/AprgCommon\",\"AprgMath/AprgMath\""
+echo "APRG_CHANGED_DIRECTORIES=[$detectedProjects]" >> "$GITHUB_OUTPUT"
