b9a3623ca0ac416a953e768a99175aca586f1ef8
mevalba <markearvinalba@gmail.com>
2023-08-15 00:01:20 +0800

    connected git checking script with github workflow yaml file

--- AllCommonScripts/DetectChangedProjects/DetectChangedProjects.sh
+++ AllCommonScripts/DetectChangedProjects/DetectChangedProjects.sh
@@ -6,33 +6,43 @@ scriptDirectory=$(dirname "$scriptPath")
 scriptName=$(basename "$scriptPath")
 aprgDirectory=$(realpath "$scriptDirectory/../../")
 aprgDirectoryName="aprg"
+declare -A cmakelistDirectoriesSet
 
 # Source needed scripts
 source "$aprgDirectory/AllCommonScripts/PrintScripts/PrintUtilities.sh"
 
 # Detect git changes
-#detectedFiles=$(git diff --name-only HEAD^)
+detectedFiles=$(git diff --name-only 07cd9c9f83422f030bff7865bb6bf58d9f9983f1 ) #HEAD^)
 
-detectedProjects=""
-searchingDirectory="$aprgDirectory/AprgCommon/AprgCommon/src/Common/Container/AlbaXY.hpp"
-while [ "$searchingDirectory" != "/" ]; do
-    scriptPrint $scriptName $LINENO "The searchingDirectory is: [$searchingDirectory]"
-	if [ $(basename "$searchingDirectory") == "$aprgDirectoryName" ]; then
-		break
-    elif [[ -e $"$searchingDirectory/CMakeLists.txt" ]]; then
-        formattedRelativePath=$(echo "\"$searchingDirectory\"" | sed -E "s|$aprgDirectory||")
-    	if [[ -z $detectedProjects ]]; then
-            detectedProjects="$formattedRelativePath"
+# Add only unique directories with CMakeLists.txt to set
+while IFS= read -r detectedFile; do
+    scriptPrint "$scriptName" "$LINENO" "Detected file: $detectedFile"
+    searchingDirectory=$(dirname "$aprgDirectory/$detectedFile")
+    while [ "$searchingDirectory" != "/" ] && [ "$searchingDirectory" != "." ]; do
+        if [ "$(basename "$searchingDirectory")" == "$aprgDirectoryName" ]; then
+            break
+        elif [[ -e "$searchingDirectory/CMakeLists.txt" ]]; then
+            formattedRelativePath=$(echo "\"$searchingDirectory\"" | sed -E "s|$aprgDirectory||")
+            scriptPrint "$scriptName" "$LINENO" "The formattedRelativePath is: [$formattedRelativePath]"
+            cmakelistDirectoriesSet["$formattedRelativePath"]=1
+            break
         else
-            detectedProjects="$detectedProjects,$formattedRelativePath"
+            searchingDirectory=$(dirname "$searchingDirectory")
         fi
-        break
-	else
-		searchingDirectory=$(dirname "$searchingDirectory")
-	fi
+    done
+done <<< "$detectedFiles"
+
+cmakelistDirectories=""
+for cmakelistDirectoriesSetItem in "${!cmakelistDirectoriesSet[@]}"; do
+    if [[ -z $cmakelistDirectories ]]; then
+        cmakelistDirectories="$cmakelistDirectoriesSetItem"
+    else
+        cmakelistDirectories="$cmakelistDirectories,$cmakelistDirectoriesSetItem"
+    fi
 done
 
-scriptPrint $scriptName $LINENO "The detectedProjects is: [$detectedProjects]"
+scriptPrint "$scriptName" "$LINENO" "The cmakelistDirectories are: [$cmakelistDirectories]"
 
 # Save the value to Github
-echo "APRG_CHANGED_DIRECTORIES=[$detectedProjects]" >> "$GITHUB_OUTPUT"
+cmakelistDirectories="\"AprgCommon/AprgCommon\",\"AprgMath/AprgMath\""
+echo "APRG_CMAKELIST_DIRECTORIES=[$cmakelistDirectories]" >> "$GITHUB_OUTPUT"
