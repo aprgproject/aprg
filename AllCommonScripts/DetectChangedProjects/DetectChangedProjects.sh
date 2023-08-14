29c3a38bedbc15a70d8e9b9d9c3e09fcf0455989
mevalba <markearvinalba@gmail.com>
2023-08-14 19:18:24 +0800

    corrected detection script

--- AllCommonScripts/DetectChangedProjects/DetectChangedProjects.sh
+++ AllCommonScripts/DetectChangedProjects/DetectChangedProjects.sh
@@ -11,9 +11,7 @@ source "$aprgDirectory/AllCommonScripts/PrintScripts/PrintUtilities.sh"
 
 # Detect git changes
 changedFiles=$(git diff --name-only HEAD^)
-scriptPrint $scriptName $LINENO "changedFiles: [$changedFiles]"
 
 # Output projects
 changedProjects="AprgCommon/AprgCommon,AprgMath/AprgMath"
-scriptPrint $scriptName $LINENO "changedProjects: [$changedProjects]"
-echo "::set-env name=ProjectsToBuildInEnv::$changedProjects"
+echo "::set-output name=ProjectsToBuild::$changedProjects"
