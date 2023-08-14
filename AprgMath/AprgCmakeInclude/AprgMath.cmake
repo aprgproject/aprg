149179f3f244fcf14952f7870c595de54e61eb4c
mevalba <markearvinalba@gmail.com>
2023-08-14 16:15:19 +0800

    made aprgmath compatible with github workflow

--- AprgMath/AprgCmakeInclude/AprgMath.cmake
+++ AprgMath/AprgCmakeInclude/AprgMath.cmake
@@ -2,7 +2,7 @@
 set(APRG_MATH_DIR ${APRG_DIR}/AprgMath/AprgMath/)
 
 #Include APRG_COMMON files and directories
-include(${APRG_MATH_DIR}/cmake/FilesAndDirectories.cmake)
+include(${APRG_MATH_DIR}/cmake/FindFilesAndDirectories.cmake)
 
 include_directories(${APRG_MATH_INCLUDE_DIRECTORIES})
 add_library(APRG_MATH ${APRG_MATH_SOURCES})
