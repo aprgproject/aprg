8aecc7dd00a42c151f8bc717bd3a10af741cc437
mevalba <markearvinalba@gmail.com>
2023-08-14 16:16:22 +0800

    fixed cmake in aprgcommon

--- AprgCommon/AprgCmakeInclude/AprgCommon.cmake
+++ AprgCommon/AprgCmakeInclude/AprgCommon.cmake
@@ -2,7 +2,7 @@
 set(APRG_COMMON_DIR ${APRG_DIR}/AprgCommon/AprgCommon/)
 
 #Include APRG_COMMON files and directories
-include(${APRG_COMMON_DIR}/cmake/FilesAndDirectories.cmake)
+include(${APRG_COMMON_DIR}/cmake/FindFilesAndDirectories.cmake)
 
 include_directories(${APRG_COMMON_INCLUDE_DIRECTORIES})
 add_library(APRG_COMMON ${APRG_COMMON_SOURCES})
