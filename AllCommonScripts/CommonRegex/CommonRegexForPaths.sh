428c1dc43c579874a9d463a9bb7be596922c5c5c
mevalba <markearvinalba@gmail.com>
2023-08-15 19:36:16 +0800

    corrected scripts

---
+++ AllCommonScripts/CommonRegex/CommonRegexForPaths.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export pythonProjectsPathRegex='/aprg/Python/'
+export cppProjectsPathSkipRegex='/aprg/Python/|/aprg/AllCommonScripts/'
+export aprgShellScriptsPathSkipRegex='/aprg/benchmark/benchmarkFromGitHub/|/aprg/gtest/gtest/|/aprg/Boost/Boost/boost/'
