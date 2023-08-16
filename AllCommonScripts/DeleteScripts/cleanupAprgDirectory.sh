c2267e7683ca381eaed8ea60c5e8ea1a64e29a12
mevalba <markearvinalba@gmail.com>
2023-08-16 17:22:38 +0800

    adding scripts and vscode configuration

---
+++ AllCommonScripts/DeleteScripts/cleanupAprgDirectory.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Set variable values
+scriptPath=$(realpath "$0")
+scriptDirectory=$(dirname "$scriptPath")
+scriptName=$(basename "$scriptPath")
+aprgDirectory=$(realpath "$scriptDirectory/../../")
+deleteDirectory="$1"
+
+deleteAllDirectoriesWithName() {
+    local nameForDeletion="$1"
+    find "$deleteDirectory" -type d -name "$nameForDeletion" -exec rm -rf {} \;
+}
+
+# Source needed scripts
+source "$aprgDirectory/AllCommonScripts/PrintScripts/PrintUtilities.sh"
+
+# Validate input
+if [ -z "$deleteDirectory" ]; then
+    deleteDirectory="$aprgDirectory"
+else
+    deleteDirectory=$(realpath "$deleteDirectory")
+fi
+if ! [[ -d $deleteDirectory ]]; then
+    scriptPrint "$scriptName" "$LINENO" "Error: The directory [$deleteDirectory] does not exist."
+    exit 1
+fi
+
+deleteAllDirectoriesWithName "VisualStudioBuild"
+deleteAllDirectoriesWithName "WslBuild"
+deleteAllDirectoriesWithName "build-*"
+
+scriptPrint "$scriptName" "$LINENO" "Cleanup completed."
