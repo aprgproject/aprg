2b4f41101995458dffcaac8083e3201411bbd526
mevalba <markearvinalba@gmail.com>
2023-08-18 01:15:12 +0800

    adding pthread flag

--- gtest/AprgCmakeInclude/gtest.cmake
+++ gtest/AprgCmakeInclude/gtest.cmake
@@ -5,7 +5,7 @@ set(GOOGLE_TEST_DIR ${GTEST_DIR}/googletest)
 
 find_package(Threads)
 if (CMAKE_USE_PTHREADS_INIT)  # The pthreads library is available and allowed.
-    set(gtest_disable_pthread ON)
+    set(gtest_disable_pthread OFF)
 else()
     set(gtest_disable_pthread ON)
 endif()
