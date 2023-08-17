a0546457e7c17b9e0ff5867c9374afd7b8d08784
mevalba <markearvinalba@gmail.com>
2023-08-17 23:25:31 +0800

    added external sacks

--- ZZZ_OldProjects/MessageContentChecker/MessageContentChecker/cmake/SpecificStepsAsExecutable.cmake
+++ ZZZ_OldProjects/MessageContentChecker/MessageContentChecker/cmake/SpecificStepsAsExecutable.cmake
@@ -1,4 +1,5 @@
 # Assign files, directories, flags, definitions and other specific steps
 
+include(${APRG_DIR}/ZZZ_OldProjects/Wbts/ExternalSacks/cmake/ExternalSacks.cmake)
 include(${MESSAGE_CONTENT_CHECKER_DIR}/cmake/FindFilesAndDirectories.cmake)
 include(${MESSAGE_CONTENT_CHECKER_DIR}/cmake/AddFlags.cmake)
