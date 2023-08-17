b2e1dd39a4c68b95c93a886f0ca35574aab71654
mevalba <markearvinalba@gmail.com>
2023-08-17 20:10:12 +0800

    dmeas correction

--- ZZZ_OldProjects/DesignDocumentCreator/DesignDocumentCreator/cmake/SpecificStepsAsExecutable.cmake
+++ ZZZ_OldProjects/DesignDocumentCreator/DesignDocumentCreator/cmake/SpecificStepsAsExecutable.cmake
@@ -1,7 +1,7 @@
 # Assign files, directories, flags, definitions and other specific steps
 
+include(${APRG_DIR}/ZZZ_OldProjects/Wbts/ExternalSacks/cmake/ExternalSacks.cmake)
 include(${DD_CREATOR_DIR}/cmake/CommonFilesAndDirectories.cmake)
-include(${DD_CREATOR_DIR}/cmake/ExternalSacks.cmake)
 set(FEATURE_FOLDER "CNI21985")
 include(${DD_CREATOR_DIR}/cmake/FeatureFilesAndDirectories.cmake)
 set(DD_CREATOR_INCLUDE_DIRECTORIES ${DD_CREATOR_DIR}/src ${DD_CREATOR_DIR}/tst)
