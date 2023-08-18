f65cf2424c519a2de7295660123b86d35ce10e98
mevalba <markearvinalba@gmail.com>
2023-08-18 10:31:05 +0800

    added fester robot in linux as excluded configuration

--- AllCommonScripts/GithubWorkflowScripts/ExcludedConfigurations.sh
+++ AllCommonScripts/GithubWorkflowScripts/ExcludedConfigurations.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export excludedConfigurations='{"project": "Chess/ChessPeek/ChessPeek", "configurationName": "UbuntuLatestGcc"}, {"project": "Chess/ChessPeek/ChessPeek", "configurationName": "UbuntuLatestClang"}, {"project": "Chess/ChessUtilities/ChessUtilities", "configurationName": "UbuntuLatestGcc"}, {"project": "Chess/ChessUtilities/ChessUtilities", "configurationName": "UbuntuLatestClang"}'
+export excludedConfigurations='{"project": "Chess/ChessPeek/ChessPeek", "configurationName": "UbuntuLatestGcc"}, {"project": "Chess/ChessPeek/ChessPeek", "configurationName": "UbuntuLatestClang"}, {"project": "Chess/ChessUtilities/ChessUtilities", "configurationName": "UbuntuLatestGcc"}, {"project": "Chess/ChessUtilities/ChessUtilities", "configurationName": "UbuntuLatestClang"}, {"project": " ZZZ_OldProjects/FesterRobot/FesterRobot", "configurationName": "UbuntuLatestGcc"}, {"project": " ZZZ_OldProjects/FesterRobot/FesterRobot", "configurationName": "UbuntuLatestClang"}'
