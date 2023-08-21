6c6e04e9a7307167e3beb57ec60136ee5afdff69
mevalba <markearvinalba@gmail.com>
2023-08-22 01:02:17 +0800

    added shell script for static analyzer

---
+++ AllCommonScripts/ScriptsForCpp/runStaticAnalyzersToAllAprgProjects.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Set variable values
+scriptPath=$(realpath "$0")
+scriptDirectory=$(dirname "$scriptPath")
+scriptName=$(basename "$scriptPath")
+aprgDirectory=$(realpath "$scriptDirectory/../../")
+directoryToConvertAllFiles="$1"
+buildAndRunScriptPath="$aprgDirectory/AllCommonScripts/BuildAndRunScripts/BuildAndRun.sh"
+
+# Use aprg directory if there are no arguments
+if [ -z "$directoryToConvertAllFiles" ]; then
+    directoryToConvertAllFiles=$aprgDirectory
+fi
+
+# Source needed scripts
+source "$aprgDirectory/AllCommonScripts/UtilitiesScripts/PrintUtilities.sh"
+aprgLocatorFile=""
+source "$aprgDirectory/AllCommonScripts/CommonRegex/AddingAprgLocatorFile.sh"
+searchCondition="*$aprgLocatorFile"
+
+# Find all files with the same name in the target folder
+scriptPrint "$scriptName" "$LINENO" "Searching all files in [$directoryToConvertAllFiles]..."
+find "$aprgDirectory" -depth -type f -wholename "$searchCondition" | while read -r aprgProjectLocatorPath; do
+    aprgProjectDirectory=$(echo "$aprgProjectLocatorPath" | sed -E "s|$aprgLocatorFile||")
+    scriptPrint "$scriptName" "$LINENO" "Searching in aprg project: [$aprgProjectDirectory]"
+    cd "$aprgProjectDirectory" || exit 1
+    "$buildAndRunScriptPath" cleanAndConfigureWithClangAndStaticAnalyzers "StaticAnalyzersBuild" "Debug" "Ninja"
+    "$buildAndRunScriptPath" build "StaticAnalyzersBuild" "Debug"
+done
+
+scriptPrint "$scriptName" "$LINENO" "All C/C++ in the directory are processed."
