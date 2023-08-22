8e3e9d71c9d35f5a64ada3e8dde35e7c70f1bca8
mevalba <markearvinalba@gmail.com>
2023-08-22 11:28:16 +0800

    added jsonlibrary in skip directories

--- AllCommonScripts/CommonRegex/CommonRegexForPaths.sh
+++ AllCommonScripts/CommonRegex/CommonRegexForPaths.sh
@@ -3,4 +3,4 @@
 export pythonProjectsPathRegex='/aprg/Python/'
 export cppProjectsPathSkipRegex='/aprg/Python/|/aprg/AllCommonScripts/'
 export shellScriptPathSkipRegex='/aprg/Python/|/aprg/Boost/|/aprg/gsl/'
-export aprgShellScriptsPathSkipRegex='/aprg/benchmark/benchmarkFromGitHub/|/aprg/Boost/Boost/boost/|/aprg/gtest/gtest/|/aprg/gsl/gsl/'
+export aprgShellScriptsPathSkipRegex='/aprg/benchmark/benchmarkFromGitHub/|/aprg/Boost/Boost/boost/|/aprg/gtest/gtest/|/aprg/gsl/gsl/|/aprg/jsonLibrary/jsonLibrary/'
