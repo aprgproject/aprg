cac3e4eefd52e103f86ec6862f181e151bf1a132
mevalba <markearvinalba@gmail.com>
2023-08-26 04:09:25 +0800

    scripts and json files improvements

--- AllCommonScripts/CopyFileScripts/copyFiles.sh
+++ AllCommonScripts/CopyFileScripts/copyFiles.sh
@@ -4,11 +4,12 @@
 scriptDirectory=$(dirname "$(realpath "$0")")
 copyCommand=$(realpath "$scriptDirectory/copyFileToSeveralLocations.sh")
 
-$copyCommand cmake/LocateAprgDirectory.cmake
-$copyCommand cmake/AddFlags.cmake
-$copyCommand .vscode/c_cpp_properties.json
-$copyCommand .vscode/launch.json
-$copyCommand .vscode/tasks.json
-$copyCommand .vscode/locateAprgDirectory.sh
-$copyCommand .vscode/runWslCommonScript.sh
-$copyCommand ProjectDirectory/CMakePresets.json
+$copyCommand CppProjects/cmake/LocateAprgDirectory.cmake
+$copyCommand CppProjects/cmake/AddFlags.cmake
+$copyCommand CppProjects/.vscode/c_cpp_properties.json
+$copyCommand CppProjects/.vscode/launch.json
+$copyCommand CppProjects/.vscode/settings.json
+$copyCommand CppProjects/.vscode/tasks.json
+$copyCommand CppProjects/.vscode/locateAprgDirectory.sh
+$copyCommand CppProjects/.vscode/runWslCommonScript.sh
+$copyCommand CppProjects/CMakePresets.json
--- AllCommonScripts/ScriptsForShellScripts/formatShellScripts.sh
+++ AllCommonScripts/ScriptsForShellScripts/formatShellScripts.sh
@@ -21,10 +21,8 @@ skipPathRegex="$aprgShellScriptsPathSkipRegex"
 # Create needed functions
 tempFile=$(mktemp)
 formatShellScriptsInDirectory() {
-    local localLintStatus
     local directoryPath
-    localLintStatus="$1"
-    directoryPath="$2"
+    directoryPath="$1"
 
     scriptPrint "$scriptName" "$LINENO" "Searching for shell scripts in: [$directoryPath]"
 
@@ -40,8 +38,6 @@ formatShellScriptsInDirectory() {
             mv "$tempFile" "$filePath"
         fi
     done < <(find "$directoryPath" -type f -name "*.sh")
-
-    return "$localLintStatus"
 }
 
 # Find all files with the same name in the target folder
