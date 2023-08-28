a4802d8606db789c17a8fde5a31b6b4e5494a5eb
mevalba <markearvinalba@gmail.com>
2023-08-28 10:05:27 +0800

    updated copy all scripts

---
+++ AllCommonScripts/CopyFileScripts/copyAllForCppProjects.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Set variable values
+scriptPath=$(realpath "$0")
+scriptDirectory=$(dirname "$scriptPath")
+scriptName=$(basename "$scriptPath")
+aprgDirectory=$(realpath "$scriptDirectory/../../")
+scriptDirectory=$(dirname "$(realpath "$0")")
+copyUsingLocatorFile=$(realpath "$scriptDirectory/copyFileUsingLocatorFile.sh")
+
+# Source needed scripts
+source "$aprgDirectory/AllCommonScripts/UtilitiesScripts/PrintUtilities.sh"
+cppSkipPathRegex=""
+source "$aprgDirectory/AllCommonScripts/CommonRegex/CommonRegexForPaths.sh"
+
+scriptPrint "$scriptName" "$LINENO" "Started copying of files for cpp projects..."
+$copyUsingLocatorFile ".*" "$cppSkipPathRegex" CppProjects/cmake/LocateAprgDirectory.cmake
+$copyUsingLocatorFile ".*" "$cppSkipPathRegex" CppProjects/.vscode/c_cpp_properties.json
+$copyUsingLocatorFile ".*" "$cppSkipPathRegex" CppProjects/.vscode/launch.json
+$copyUsingLocatorFile ".*" "$cppSkipPathRegex" CppProjects/.vscode/settings.json
+$copyUsingLocatorFile ".*" "$cppSkipPathRegex" CppProjects/.vscode/tasks.json
+$copyUsingLocatorFile ".*" "$cppSkipPathRegex" CppProjects/.vscode/locateAprgDirectory.sh
+$copyUsingLocatorFile ".*" "$cppSkipPathRegex" CppProjects/.vscode/runWslCommonScript.sh
+$copyUsingLocatorFile ".*" "$cppSkipPathRegex" CppProjects/CMakePresets.json
+scriptPrint "$scriptName" "$LINENO" "The copying of files for cpp projects is finished."
