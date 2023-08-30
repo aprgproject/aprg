c7aa6eae674fb5142ea08ff880ade610bc071751
mevalba <markearvinalba@gmail.com>
2023-08-30 23:38:11 +0800

    transferred gnuplot wrapper to another folder to skip clang tidy checks

--- AprgGnuPlot/AprgGnuPlot/cmake/FindFilesAndDirectories.cmake
+++ AprgGnuPlot/AprgGnuPlot/cmake/FindFilesAndDirectories.cmake
@@ -2,14 +2,35 @@ set(APRG_GNU_PLOT_SOURCE_DIRECTORY ${APRG_GNU_PLOT_DIR}/src)
 
 set(APRG_GNU_PLOT_TEST_DIRECTORY ${APRG_GNU_PLOT_DIR}/tst)
 
-set(APRG_GNU_PLOT_INCLUDE_DIRECTORIES ${APRG_GNU_PLOT_SOURCE_DIRECTORY})
+set(GNU_PLOT_WRAPPER_SOURCE_DIRECTORY ${GNU_PLOT_WRAPPER_DIR}/src)
 
-set(APRG_GNU_PLOT_TEST_INCLUDE_DIRECTORIES ${APRG_GNU_PLOT_TEST_DIRECTORY})
+set(GNU_PLOT_WRAPPER_TEST_DIRECTORY ${GNU_PLOT_WRAPPER_DIR}/tst)
 
-print_status("Looking for sources C/C++ files in: [${APRG_GNU_PLOT_SOURCE_DIRECTORY}]")
-get_source_files_from_directory(APRG_GNU_PLOT_SOURCES ${APRG_GNU_PLOT_SOURCE_DIRECTORY})
+set(APRG_GNU_PLOT_INCLUDE_DIRECTORIES ${APRG_GNU_PLOT_SOURCE_DIRECTORY}
+                                      ${GNU_PLOT_WRAPPER_SOURCE_DIRECTORY})
 
-print_status("Looking for tests C/C++ files in: [${APRG_GNU_PLOT_TEST_DIRECTORY}]")
-get_source_files_from_directory(APRG_GNU_PLOT_TESTS ${APRG_GNU_PLOT_TEST_DIRECTORY})
+set(APRG_GNU_PLOT_TEST_INCLUDE_DIRECTORIES ${APRG_GNU_PLOT_TEST_DIRECTORY}
+                                           ${GNU_PLOT_WRAPPER_TEST_DIRECTORY})
 
-set(APRG_GNU_PLOT_SOURCES_AND_TESTS ${APRG_GNU_PLOT_SOURCES} ${APRG_GNU_PLOT_TESTS})
+print_status(
+  "Looking for sources C/C++ files in: [${APRG_GNU_PLOT_SOURCE_DIRECTORY}]")
+get_source_files_from_directory(APRG_GNU_PLOT_SOURCES
+                                ${APRG_GNU_PLOT_SOURCE_DIRECTORY})
+
+print_status(
+  "Looking for sources C/C++ files in: [${GNU_PLOT_WRAPPER_SOURCE_DIRECTORY}]")
+get_source_files_from_directory(APRG_GNU_PLOT_SOURCES
+                                ${GNU_PLOT_WRAPPER_SOURCE_DIRECTORY})
+
+print_status(
+  "Looking for tests C/C++ files in: [${APRG_GNU_PLOT_TEST_DIRECTORY}]")
+get_source_files_from_directory(APRG_GNU_PLOT_TESTS
+                                ${APRG_GNU_PLOT_TEST_DIRECTORY})
+
+print_status(
+  "Looking for tests C/C++ files in: [${GNU_PLOT_WRAPPER_TEST_DIRECTORY}]")
+get_source_files_from_directory(APRG_GNU_PLOT_TESTS
+                                ${GNU_PLOT_WRAPPER_TEST_DIRECTORY})
+
+set(APRG_GNU_PLOT_SOURCES_AND_TESTS ${APRG_GNU_PLOT_SOURCES}
+                                    ${APRG_GNU_PLOT_TESTS})
