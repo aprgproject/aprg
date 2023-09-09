9829145be46cb213d9ac029614b0d2d1ac5f7920
mevalba <markearvinalba@gmail.com>
2023-09-10 00:35:24 +0800

    used NAN and infinity in numeric limits

--- AprgCommon/AprgCommon/src/Common/Math/AlbaMathConstants.hpp
+++ AprgCommon/AprgCommon/src/Common/Math/AlbaMathConstants.hpp
@@ -19,9 +19,9 @@ constexpr double E_DOUBLE_VALUE = 2.7182818284590452354;
 // Example:
 // -> template <typename ConstType>
 // -> constexpr ConstType PI_DOUBLE_VALUE(3.14159265358979323846);
-constexpr double POSITIVE_INFINITY_DOUBLE_VALUE = static_cast<double>(INFINITY);
-constexpr double NEGATIVE_INFINITY_DOUBLE_VALUE = static_cast<double>(-INFINITY);
-constexpr double NAN_DOUBLE_VALUE = static_cast<double>(NAN);
+constexpr double POSITIVE_INFINITY_DOUBLE_VALUE = std::numeric_limits<double>::infinity();
+constexpr double NEGATIVE_INFINITY_DOUBLE_VALUE = -std::numeric_limits<double>::infinity();
+constexpr double NAN_DOUBLE_VALUE = std::numeric_limits<double>::quiet_NaN();
 constexpr double DOUBLE_EPSILON = std::numeric_limits<double>::epsilon();
 }  // namespace alba::AlbaMathConstants
 
