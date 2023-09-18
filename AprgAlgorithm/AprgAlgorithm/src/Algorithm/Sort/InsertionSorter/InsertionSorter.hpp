2ccd427bc20f41573352d295334b753459b8e602
mevalba <markearvinalba@gmail.com>
2023-09-18 18:09:18 +0800

    added more debug steps

--- AprgAlgorithm/AprgAlgorithm/src/Algorithm/Sort/InsertionSorter/InsertionSorter.hpp
+++ AprgAlgorithm/AprgAlgorithm/src/Algorithm/Sort/InsertionSorter/InsertionSorter.hpp
@@ -42,10 +42,18 @@ private:
             ALBA_DBG_PRINT(valuesToSort);
             ALBA_DBG_PRINT(*rItLow, *rItHigh);
         }*/
-        for (; rItLow != valuesToSort.rend() && *rItLow > *rItHigh; ++rItLow, ++rItHigh) {
+
+        for (; rItLow != valuesToSort.rend() && rItHigh != valuesToSort.rend() && *rItLow > *rItHigh;
+             ++rItLow, ++rItHigh) {
             ALBA_DBG_PRINT(valuesToSort);
             ALBA_DBG_PRINT(*rItLow, *rItHigh);
         }
+
+        // error happens here
+        /*for (; rItLow != valuesToSort.rend() && *rItLow > *rItHigh; ++rItLow, ++rItHigh) {
+            ALBA_DBG_PRINT(valuesToSort);
+            ALBA_DBG_PRINT(*rItLow, *rItHigh);
+        }*/
         /*for (; rItLow != valuesToSort.rend() && *rItLow > *rItHigh; ++rItLow, ++rItHigh) {
             ALBA_DBG_PRINT(valuesToSort);
             ALBA_DBG_PRINT(*rItLow, *rItHigh);
