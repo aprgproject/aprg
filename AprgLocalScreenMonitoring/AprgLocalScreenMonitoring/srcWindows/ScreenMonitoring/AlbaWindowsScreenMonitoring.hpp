cc7d9ef93524600b38c597cd83514aa1af4cfc57
mevalba <markearvinalba@gmail.com>
2023-09-23 05:37:06 +0800

    fixed cppcoreguidelines-special-member-functions hicpp-special-member-functions

--- AprgLocalScreenMonitoring/AprgLocalScreenMonitoring/srcWindows/ScreenMonitoring/AlbaWindowsScreenMonitoring.hpp
+++ AprgLocalScreenMonitoring/AprgLocalScreenMonitoring/srcWindows/ScreenMonitoring/AlbaWindowsScreenMonitoring.hpp
@@ -10,6 +10,10 @@ namespace alba {
 
 class AlbaWindowsScreenMonitoring {
 public:
+    AlbaWindowsScreenMonitoring(AlbaWindowsScreenMonitoring const &) = default;
+    AlbaWindowsScreenMonitoring(AlbaWindowsScreenMonitoring &&) = default;
+    AlbaWindowsScreenMonitoring &operator=(AlbaWindowsScreenMonitoring const &) = default;
+    AlbaWindowsScreenMonitoring &operator=(AlbaWindowsScreenMonitoring &&) = default;
     AlbaWindowsScreenMonitoring();
     ~AlbaWindowsScreenMonitoring();
     [[nodiscard]] uint32_t getColorAt(int const x, int const y) const;
@@ -21,7 +25,7 @@ private:
     void cleanup();
     void initializeScreenParameters();
     void saveBitmapInfo(HDC const screenHandler, HBITMAP const bitmapHandler);
-    void savePixelsFromBitmapScreen(HDC const screenHandler, HBITMAP const bitmapHandler, BITMAPINFO& bitmapInfo);
+    void savePixelsFromBitmapScreen(HDC const screenHandler, HBITMAP const bitmapHandler, BITMAPINFO &bitmapInfo);
     int m_screenWidth;
     int m_screenHeight;
     HDC m_screenHandler;
