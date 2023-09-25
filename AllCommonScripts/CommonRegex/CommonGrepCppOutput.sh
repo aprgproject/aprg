abfb55ef3965b4bb3e0b09ca8ffe6b17b39fb837
mevalba <markearvinalba@gmail.com>
2023-09-25 16:24:45 +0800

    made a common grep for cpp output for scripts

---
+++ AllCommonScripts/CommonRegex/CommonGrepCppOutput.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export cppItemWithSeverityGrep='note:|style:|warning:|error:'
+export cppItemAprgOnlyGrep='benchmark::|testing::|TestFactoryBase|test_info_|(\/|\\)aprg(\/|\\)benchmark(\/|\\)benchmarkLibrary(\/|\\)|(\/|\\)aprg(\/|\\)gtest(\/|\\)gtest(\/|\\)|(\/|\\)aprg(\/|\\)gsl(\/|\\)gsl(\/|\\)'
+
